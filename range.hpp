ef11e1d82a641085df223c8ec0949dca6d7c7146
Ryan Haining <rhainin1@binghamton.edu>
2013-09-16 13:12:18 -0700

    Merged range.h and range.cpp into range.hpp

--- range.hpp
+++ range.hpp
@@ -1,6 +1,59 @@
-#include "range.h"
+#ifndef __RANGE__H__
+#define __RANGE__H__
+
+// range() for range-based loops with start, stop, and step.
+//
+// Acceptable forms are:
+// for (auto i : range(stop)) { ... } // start = 0, step = 1
+// for (auto i : range(start, stop)) { ... } // step = 1
+// for (auto i : range(start, stop, step)) { ... }
+//
+// The start may be greater than the stop if the range is negative
+// The range will effectively be empty if:
+//   1) step is positive and start > stop
+//   2) step is negative and start < stop
+//
+// If a step of 0 is provided, a RangeException will be thrown
+
+#include <exception>
 
 namespace iter {
+
+    // Thrown when step 0 occurs
+    class RangeException : public std::exception {
+        virtual const char *what() const throw() { 
+            return "range() step argument must not be zero";
+        }
+    };
+
+    class range {
+        private:
+            const int start; 
+            const int stop;
+            const int step;
+            void step_check() const throw(RangeException);
+        public:
+            class Iterator {
+                private:
+                    int value;
+                    const int step;
+                public:
+                    Iterator (int val, int step);
+
+                    int operator*() const;
+                    Iterator & operator++();
+                    bool operator!=(const Iterator & other) const;
+            };
+
+            range(int stop);
+            range(int start, int stop);
+            range(int start, int stop, int step);
+
+            Iterator begin() const;
+            Iterator end() const;
+    };
+
+    // definitions
 
     // Iterator subclass
     range::Iterator::Iterator (int val, int step) :
@@ -68,3 +121,5 @@ namespace iter {
     }
 
 }
+
+#endif //ifndef __RANGE__H__
