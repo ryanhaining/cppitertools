a43e9f2b4a4baa297985ea0c981e3f3ee641ea29
Ryan Haining <rhainin1@binghamton.edu>
2013-09-17 13:10:45 -0700

    Adds callable object test to testfilter

--- tests/testfilter.cpp
+++ tests/testfilter.cpp
@@ -9,17 +9,37 @@ bool greater_than_four(int i) {
     return i > 4;
 }
 
-int main() {
-    std::vector<int> vec{1, 5, 6, 7, 2, 8, 3, 2, 1};
+class LessThanValue {
+    private:
+        int compare_val;
 
-    std::cout << "Greater than 4\n";
+    public:
+        LessThanValue() = delete;
+        LessThanValue(int v) : compare_val(v) { }
+        
+        bool operator() (int i) {
+            return i < this->compare_val;
+        }
+};
+
+
+int main() {
+    std::vector<int> vec{1, 5, 6, 7, 2, 3, 8, 3, 2, 1};
+
+    std::cout << "Greater than 4 (function pointer)\n";
     for (auto i : filter(greater_than_four, vec)) {
         std::cout << i << '\n';
     }
 
-    std::cout << "Less than 4\n";
+    std::cout << "Less than 4 (lambda)\n";
     for (auto i : filter([] (const int i) { return i < 4; }, vec)) {
-        std::cout << i <<'\n';
+        std::cout << i << '\n';
+    }
+
+    LessThanValue lv(4);
+    std::cout << "Less than 4 (callable object)\n";
+    for (auto i : filter(lv, vec)) {
+        std::cout << i << '\n';
     }
 
     return 0;
