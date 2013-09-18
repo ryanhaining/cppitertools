11f93dbfe41932eb53e4e016ae8b0f19825ee74e
Ryan Haining <rhainin1@binghamton.edu>
2013-09-17 17:25:22 -0700

    Formats wrap_iter.hpp

--- wrap_iter.hpp
+++ wrap_iter.hpp
@@ -1,32 +1,39 @@
-#ifndef WRAP_ITER_HPP
-#define WRAP_ITER_HPP
+#ifndef WRAP_ITER_HPP__
+#define WRAP_ITER_HPP__
 
 namespace iter {
     template <typename Iterator>
-        struct wrap_iter {
+    struct wrap_iter {
+        public:
             Iterator iter;
             typename std::iterator_traits<Iterator>::difference_type step;
+
             wrap_iter(const Iterator & iter,
                     typename std::iterator_traits<Iterator>::difference_type step) : 
                 iter(iter),
-                step(step) {}
+                step(step)
+            { }
+
             wrap_iter & operator++() {
                 iter += step;
                 return *this;
             }
+
             bool operator!=(const wrap_iter & rhs) {
                 return this->iter != rhs.iter;
             }
+
             auto operator*() -> decltype(*iter)
             {
                 return *iter;
             }
-        };
+    };
+
     template <typename Iterator>
-        wrap_iter<Iterator> make_wrap_iter(const Iterator & iter,
-                typename std::iterator_traits<Iterator>::difference_type step) {
-            return wrap_iter<Iterator>(iter,step);
-        }
+    wrap_iter<Iterator> make_wrap_iter(const Iterator & iter,
+            typename std::iterator_traits<Iterator>::difference_type step) {
+        return wrap_iter<Iterator>(iter,step);
+    }
 }
 
-#endif //WRAP_ITER_HPP
+#endif //WRAP_ITER_HPP__
