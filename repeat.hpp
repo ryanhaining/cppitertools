d10aa54e6df484c243175c09e688171e7cf3f219
Ryan Haining <rhainin1@binghamton.edu>
2013-09-18 19:40:37 -0700

    Various touchups to repeat
    Adds const-correctess, this->, refactors operator!= to one line.

--- repeat.hpp
+++ repeat.hpp
@@ -1,45 +1,48 @@
 #ifndef REPEAT_HPP
 #define REPEAT_HPP
 
-#include <iostream>
 #include <iterator_range.hpp>
+
 #include <iterator>
 
 namespace iter {
     template <typename Elem>
-        struct repeat_iter {
+    class repeat_iter {
+        private:
             const Elem elem;
             size_t until;
             size_t start=0;
-            repeat_iter(const Elem & elem,
-                    size_t until) : 
+        public:
+            repeat_iter(const Elem & elem, size_t until) : 
                 elem(elem),
-                until(until) {}
+                until(until)
+            { }
+
             repeat_iter & operator++() {
-                if (until != 0) ++start; //no point in repeating 0 times
+                if (this->until != 0) ++start; //no point in repeating 0 times
                 //so use that to repeat infinitely
                 return *this;
             }
-            bool operator!=(const repeat_iter &) {
-                if (until == 0) return true;
-                else if (start != until) return true;
-                else return false;
+
+            bool operator!=(const repeat_iter &) const {
+                return this->until == 0 || this->start != this->until;
             }
-            Elem operator*()
-            {
-                return elem;
+
+            Elem operator*() const {
+                return this->elem;
             }
-        };
+    };
+
+    //-1 causes it to repeat infintely 
     template <typename Elem>
-        iterator_range<repeat_iter<Elem>> repeat(
-                const Elem & elem,
-                size_t until=0)
-        //-1 causes it to repeat infintely 
-        {
-            return iterator_range<repeat_iter<Elem>>(
-                    repeat_iter<Elem>(elem,until),
-                    repeat_iter<Elem>(elem,until));//just a dummy iter
-        }
+    iterator_range<repeat_iter<Elem>> repeat(
+            const Elem & elem,
+            size_t until=0)
+    {
+        return iterator_range<repeat_iter<Elem>>(
+                repeat_iter<Elem>(elem, until),
+                repeat_iter<Elem>(elem, until));//just a dummy iter
+    }
         
 }
 
