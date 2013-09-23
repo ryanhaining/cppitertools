36287eb38272c19d34e614f987fb928f26d4daed
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-23 18:17:06 -0400

    Made some fixes on teszip_longest and added it to make file

--- tests/testzip_longest.cpp
+++ tests/testzip_longest.cpp
@@ -14,7 +14,7 @@ std::ostream & operator<<(std::ostream & o, const boost::optional<T> & opt) {
         std::cout << **opt << std::endl;
     }
     else {
-        std::cout << "Object disengaged" << std::endl;
+        std::cout << "Object disengaged of type " << typeid(T).name() << std::endl;
     }
     return o;
 }
@@ -39,39 +39,39 @@ int main() {
         std::array<double,5> d{{1.2,1.2,1.2,1.2,1.2}};
         std::cout << std::endl << "Variadic template zip_longest" << std::endl;
         for (auto e : iter::zip_longest(i,f,s,d)) {
-            std::cout << std::get<0>(e) << " " 
-                << std::get<1>(e) << " " 
-                << std::get<2>(e) << " "
+            std::cout << std::get<0>(e)
+                << std::get<1>(e) 
+                << std::get<2>(e) 
                 << std::get<3>(e) << std::endl;
             **std::get<1>(e)=2.2f; //modify the float array
         }
         std::cout<<std::endl;
         for (auto e : iter::zip_longest(i,s,f,d)) {
-            std::cout << std::get<0>(e) << " " 
-                << std::get<1>(e) << " "
-                << std::get<2>(e) << " " 
+            std::cout << std::get<0>(e) 
+                << std::get<1>(e) 
+                << std::get<2>(e) 
                 << std::get<3>(e) << std::endl;
         }
         std::cout << std::endl << "Try some weird range differences" << std::endl;
         std::vector<int> empty{};
         for (auto e : iter::zip_longest(empty,f,s,d)) {
-            std::cout << std::get<0>(e) << " " 
-                << std::get<1>(e) << " " 
-                << std::get<2>(e) << " "
+            std::cout << std::get<0>(e) 
+                << std::get<1>(e) 
+                << std::get<2>(e) 
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
         for (auto e : iter::zip_longest(f,empty,s,d)) {
-            std::cout << std::get<0>(e) << " " 
-                << std::get<1>(e) << " " 
-                << std::get<2>(e) << " "
+            std::cout << std::get<0>(e) 
+                << std::get<1>(e) 
+                << std::get<2>(e) 
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
         for (auto e : iter::zip_longest(f,s,i,d)) { 
-            std::cout << std::get<0>(e) << " " 
-                << std::get<1>(e) << " " 
-                << std::get<2>(e) << " "
+            std::cout << std::get<0>(e) 
+                << std::get<1>(e) 
+                << std::get<2>(e) 
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
