5a9e1437c77204c85f516166627824a56c419bfd
Ryan Haining <rhainin1@binghamton.edu>
2013-09-23 01:20:18 -0700

    Adds dropwhile
    Drops all elements at the beginning of a sequence which are true under
    the predicate.

---
+++ dropwhile.hpp
@@ -0,0 +1,106 @@
+#ifndef DROPWHILE__H__
+#define DROPWHILE__H__
+
+namespace iter {
+
+    //Forward declarations of DropWhile and dropwhile
+    template <typename FilterFunc, typename Container>
+    class DropWhile;
+
+    template <typename FilterFunc, typename Container>
+    DropWhile<FilterFunc, Container> dropwhile(FilterFunc, Container &);
+
+    template <typename FilterFunc, typename Container>
+    class DropWhile {
+        friend DropWhile dropwhile<FilterFunc, Container>(
+                FilterFunc, Container &);
+
+        // Type of the Container::Iterator, but since the name of that 
+        // iterator can be anything, we have to grab it with this
+        using contained_iter_type =
+            decltype(((Container *)nullptr)->begin());
+
+        // The type returned when dereferencing the Container::Iterator
+        using contained_iter_ret =
+            decltype(((contained_iter_type *)nullptr)->operator*());
+
+        private:
+            Container & container;
+            FilterFunc filter_func;
+            
+            // Value constructor for use only in the dropwhile function
+            DropWhile(FilterFunc filter_func, Container & container) :
+                container(container),
+                filter_func(filter_func)
+            { }
+            DropWhile () = delete;
+            DropWhile & operator=(const DropWhile &) = delete;
+            // Default copy constructor used
+
+        public:
+            class Iterator {
+                private:
+                    contained_iter_type sub_iter;
+                    const contained_iter_type sub_end;
+                    FilterFunc filter_func;
+
+                    // increment until the iterator points to is true on the 
+                    // predicate.  Called by constructor and operator++
+                    void skip_failures() { 
+                        while (this->sub_iter != this->sub_end
+                                && this->filter_func(*this->sub_iter)) {
+                            ++this->sub_iter;
+                        }
+                    }
+
+                public:
+                    Iterator (contained_iter_type iter,
+                            contained_iter_type end,
+                            FilterFunc filter_func) :
+                        sub_iter(iter),
+                        sub_end(end),
+                        filter_func(filter_func)
+                    { 
+                        this->skip_failures();
+                    } 
+
+                    contained_iter_ret operator*() const {
+                        return *this->sub_iter;
+                    }
+
+                    Iterator & operator++() { 
+                        ++this->sub_iter;
+                        return *this;
+                    }
+
+                    bool operator!=(const Iterator & other) const {
+                        return this->sub_iter != other.sub_iter;
+                    }
+            };
+
+            Iterator begin() const {
+                return Iterator(
+                        this->container.begin(),
+                        this->container.end(),
+                        this->filter_func);
+            }
+
+            Iterator end() const {
+                return Iterator(
+                        this->container.end(),
+                        this->container.end(),
+                        this->filter_func);
+            }
+
+    };
+
+    // Helper function to instantiate a DropWhile
+    template <typename FilterFunc, typename Container>
+    DropWhile<FilterFunc, Container> dropwhile(
+            FilterFunc filter_func, Container & container) {
+        return DropWhile<FilterFunc, Container>(filter_func, container);
+    }
+
+}
+
+#endif //ifndef DROPWHILE__H__
