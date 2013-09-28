5f139b1764afea8a29f171d46433ce0b25ced1f1
Aaron Josephs <ajoseph4@binghamton.edu>
2013-09-28 13:55:03 -0400

    Added all files to itertools.hpp

--- itertools.hpp
+++ itertools.hpp
@@ -5,8 +5,26 @@
 #include "reverse.hpp"
 #include "slice.hpp"
 #include "chain.hpp"
+#include "chain.hpp"
+#include "combinations_with_replacement.hpp"
+#include "cycle.hpp"
+#include "dropwhile.hpp"
+#include "enumerate.hpp"
+#include "filter.hpp"
+#include "iterator_range.hpp"
+#include "permutations.hpp"
+#include "product.hpp"
+#include "range.hpp"
+#include "repeat.hpp"
+#include "reverse.hpp"
+#include "slice.hpp"
+#include "takewhile.hpp"
+#include "wrap_iter.hpp"
+#include "zip.hpp"
+#include "zip_longest.hpp"
 
 //not sure if should include "iterator_range.hpp"
 //since it's already in everything
 
 #endif
+
