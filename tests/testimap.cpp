094aff499a1998206edf2d30baf3518170fccf96
Ryan Haining <rhainin1@binghamton.edu>
2013-10-05 02:26:26 -0700

    adds the rest of the tests to testimap

--- tests/testimap.cpp
+++ tests/testimap.cpp
@@ -11,5 +11,16 @@ int main() {
     for (auto i : imap([] (int x, int y) { return x + y; }, vec1, vec2)) {
         std::cout << i << '\n';
     }
+
+    std::vector<int> vec3 = {100, 200, 300, 400, 500, 600};
+    for (auto i : imap([] (int a, int b, int c) { return a + b + c; }, 
+                vec1, vec2, vec3)) {
+        std::cout << i << '\n';
+    }
+
+    for (auto i : imap([] (int i) {return i * i; }, vec1)) {
+        std::cout << i << '\n';
+    }
+
     return 0;
 }
