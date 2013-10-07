e9b6a707761e3803993d57e79aa045384df41fcf
Aaron Josephs <ajoseph4@binghamton.edu>
2013-10-06 22:38:43 -0400

    fixed slight error in grouper if given an empty range

--- tests/testgrouper.cpp
+++ tests/testgrouper.cpp
@@ -3,13 +3,27 @@
 #include <vector>
 using iter::grouper;
 int main() {
-    std::vector<int> v = {1,2,3,4,5,6,7,8,9}; 
+    std::vector<int> v {1,2,3,4,5,6,7,8,9}; 
     for (auto sec : grouper(v,4)) {
+        for (auto i : sec) {
+            std::cout << i << " ";
+            i.get() *= 2;
+        }
+        std::cout << std::endl;
+    }
+    for (auto sec : grouper(v,3)) {
         for (auto i : sec) {
             std::cout << i << " ";
             //i.get() = 90;
         }
         std::cout << std::endl;
+    }
+    std::vector<int> empty {};
+    for (auto sec : grouper(empty,3)) {
+        std::cout << "Shouldn't print\n";
+        for (auto i : sec) {
+            std::cout << i << " Shouldn't print\n";
+        }
     }
     return 0;
 }
