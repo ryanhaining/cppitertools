b537ab010e04b2279d21609297769c8aff600578
Ryan Haining <rhainin1@binghamton.edu>
2013-10-07 02:12:55 -0700

    Formatting on grouper

--- grouper.hpp
+++ grouper.hpp
@@ -9,56 +9,72 @@
 
 namespace iter {
     template <typename Container>
-        struct grouper_iter;
-    template <typename Container>
-        iterator_range<grouper_iter<Container>>
-        grouper(Container & container, size_t s) {
-            auto begin = grouper_iter<Container>(container,s);
-            auto end = grouper_iter<Container>(container);
-            return iterator_range<grouper_iter<Container>>(begin,end);
-        }
+    class grouper_iter;
 
     template <typename Container>
-        struct grouper_iter {
+    iterator_range<grouper_iter<Container>> grouper(
+            Container & container, size_t s) {
+        auto begin = grouper_iter<Container>(container, s);
+        auto end = grouper_iter<Container>(container);
+        return iterator_range<grouper_iter<Container>>(begin, end);
+    }
+
+    template <typename Container>
+    class grouper_iter {
+        private:
             Container & container;
             using Iterator = decltype(container.begin());
+            using Deref_type =
+                std::vector<
+                    std::reference_wrapper<
+                        typename std::remove_reference<
+                            decltype(*std::declval<Iterator>())>::type>>;
+
+
             std::vector<Iterator> group;
+
             size_t group_size = 0;
             bool not_done = true;
+
+        public: 
             grouper_iter(Container & c, size_t s) :
                 container(c),group_size(s) 
             {
-                //if the group size is 0 or the container is empty produce nothing
-                if (group_size == 0 || !(container.begin() != container.end())) {
-                    not_done = false;
+                // if the group size is 0 or the container is empty produce
+                // nothing
+                if (this->group_size == 0 ||
+                        !(this->container.begin() != this->container.end())) {
+                    this->not_done = false;
                     return;
                 }
-                for (size_t i = 0; i < group_size; ++i) 
-                    group.push_back(container.begin()+i);
+                for (size_t i = 0; i < this->group_size; ++i) 
+                    this->group.push_back(this->container.begin() + i);
             }
-            //seems like constructor is same as moving_section_iter
-            grouper_iter(Container & c) : container(c)
-            //creates the end iterator
+
+            //seems like conclassor is same as moving_section_iter
+            grouper_iter(Container & c) :
+                container(c)
             {
+                //creates the end iterator
                 group.push_back(container.end());
             }
 
             grouper_iter & operator++() {
-                for (auto & iter : group) {
-                    iter += group_size;
+                for (auto & iter : this->group) {
+                    iter += this->group_size;
                 }
                 return *this;
             }
-            bool operator!=(const grouper_iter &) {
-                return not_done;
+
+            bool operator!=(const grouper_iter &) const {
+                return this->not_done;
             }
-            using Deref_type = std::vector<std::reference_wrapper<typename std::remove_reference<decltype(*std::declval<Iterator>())>::type>>;
-            Deref_type operator*()
-            {
+
+            Deref_type operator*() {
                 Deref_type vec;
-                for (auto i : group) {
-                    if(i == container.end()) {
-                        not_done = false;
+                for (auto i : this->group) {
+                    if(i == this->container.end()) {
+                        this->not_done = false;
                         break;
                     } 
                     //if the group is at the end the vector will be smaller 
@@ -68,7 +84,7 @@ namespace iter {
                 }
                 return vec;
             }
-        };
+    };
 }
 
-#endif //MOVING_SECTION_HPP
+#endif // ifndef GROUPER_HPP
