f9c967cbcba2c11cb750c8ec00662e3be56e2a72
Ryan Haining <rhainin1@binghamton.edu>
2013-10-08 01:02:52 -0700

    Adds case for only list passed to filterfalse

--- tests/testfilterfalse.cpp
+++ tests/testfilterfalse.cpp
@@ -42,5 +42,11 @@ int main() {
         std::cout << i << '\n';
     }
 
+    std::cout << "Nonzero ints filter(vec2)\n";
+    std::vector<int> vec2 {0, 1, 2, 0, 3, 0, 0, 0, 4, 5, 0};
+    for (auto i : filterfalse(vec2)) {
+        std::cout << i << '\n';
+    }
+
     return 0;
 }
