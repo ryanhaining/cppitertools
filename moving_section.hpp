38490c814ccaa2c5863a6abb203c2524c67638f3
Aaron Josephs <ajoseph4@binghamton.edu>
2013-10-10 16:08:09 -0400

    perfect forwarding in moving_section and reverse

--- moving_section.hpp
+++ moving_section.hpp
@@ -6,30 +6,31 @@
 #include <algorithm>
 #include <functional>
 #include <type_traits>
+#include <utility>
 
 namespace iter {
     template <typename Container>
         struct moving_section_iter;
     template <typename Container>
         iterator_range<moving_section_iter<Container>>
-        moving_section(Container & container, size_t s) {
-            auto begin = moving_section_iter<Container>(container,s);
-            auto end = moving_section_iter<Container>(container);
+        moving_section(Container && container, size_t s) {
+            auto begin = moving_section_iter<Container>(std::forward<Container>(container),s);
+            auto end = moving_section_iter<Container>(std::forward<Container>(container));
             return iterator_range<moving_section_iter<Container>>(begin,end);
         }
 
     template <typename Container>
         struct moving_section_iter {
-            Container & container;
+            Container && container;
             using Iterator = decltype(container.begin());
             std::vector<Iterator> section;
             size_t section_size = 0;
-            moving_section_iter(Container & c, size_t s) :
+            moving_section_iter(Container && c, size_t s) :
                 container(c),section_size(s) {
                     for (size_t i = 0; i < section_size; ++i) 
                         section.push_back(container.begin()+i);
                 }
-            moving_section_iter(Container & c) : container(c)
+            moving_section_iter(Container && c) : container(c)
             //creates the end iterator
             {
                 section.push_back(container.end());
