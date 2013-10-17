485e9e894f70bd3c601bf26f120d6a1a67c811e2
Ryan Haining <rhainin1@binghamton.edu>
2013-10-17 02:22:58 -0700

    Formats zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -1,28 +1,35 @@
 #ifndef ZIP_HPP
 #define ZIP_HPP
 
-#include <tuple>
 #include "iterator_range.hpp"
+
+#include <tuple>
 
 namespace iter {
     template <typename ... Rest>
-        struct zip_iter;
-    template <typename ... Containers>
-        auto zip(Containers && ... containers) ->
-        iterator_range<zip_iter<decltype(containers.begin())...>>
-        {
-            auto begin = zip_iter<decltype(containers.begin())...>(containers.begin()...);
-            auto end = zip_iter<decltype(containers.end())...>(containers.end()...);
-            return iterator_range<decltype(begin)>(begin,end);
-        }
-    template <typename Iterator>
-        struct zip_iter<Iterator> {
+    class zip_iter;
 
+    template <typename ... Containers>
+    auto zip(Containers && ... containers) ->
+            iterator_range<zip_iter<decltype(containers.begin())...>>
+    {
+        auto begin =
+            zip_iter<decltype(containers.begin())...>(containers.begin()...);
+
+        auto end =
+            zip_iter<decltype(containers.end())...>(containers.end()...);
+
+        return iterator_range<decltype(begin)>(begin,end);
+    }
+
+
+    template <typename Iterator>
+    class zip_iter<Iterator> {
         private:
             Iterator iter;
 
         public:
-            using Elem_t = decltype(*iter);
+            using elem_type = decltype(*iter);
             zip_iter(const Iterator & i) :
                 iter(i){ }
 
@@ -37,7 +44,8 @@ namespace iter {
             bool operator!=(const zip_iter & rhs) const {
                 return (this->iter != rhs.iter);
             }
-        };  /* 
+    };
+#if 0
     template <typename First, typename Second>
         struct zip_iter<First,Second> {
 
@@ -63,18 +71,18 @@ namespace iter {
             bool operator!=(const zip_iter & rhs) const {
                 return (this->iter1 != rhs.iter1) && (this->iter2 != rhs.iter2);
             }
-        };*/
+        };
+#endif
     //this specialization commented out 
     template <typename First, typename ... Rest>
-        struct zip_iter<First,Rest...> {
-
+    class zip_iter<First,Rest...> {
         private:
             First iter;
             zip_iter<Rest...> inner_iter;
             
         public:
-            using Elem_t = decltype(*iter);
-            using tuple_t = 
+            using elem_type = decltype(*iter);
+            using tuple_type = 
                 decltype(std::tuple_cat(std::tie(*iter),*inner_iter));
 
             zip_iter(const First & f, const Rest & ... rest) :
@@ -82,19 +90,22 @@ namespace iter {
                 inner_iter(rest...) {}
 
 
-            tuple_t operator*()
+            tuple_type operator*()
             {
                 return std::tuple_cat(std::tie(*iter),*inner_iter);
             }
+
             zip_iter & operator++() {
                 ++iter;
                 ++inner_iter;
                 return *this;
             }
+
             bool operator!=(const zip_iter & rhs) const {
-                return (this->iter != rhs.iter) && (this->inner_iter != rhs.inner_iter);
+                return (this->iter != rhs.iter) &&
+                    (this->inner_iter != rhs.inner_iter);
             }
-        };
+    };
 }
 
 #endif //ZIP_HPP
