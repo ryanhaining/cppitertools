50685116e7e9bd9be627bfd963fe395d5fbd99e7
Aaron Josephs <ajoseph4@binghamton.edu>
2013-11-07 18:20:48 -0500

    updated itertools.hpp to include everything '

--- itertools.hpp
+++ itertools.hpp
@@ -1,34 +1,39 @@
 #ifndef ITERTOOLS_HPP
 #define ITERTOOLS_HPP
 
-#include "zip.hpp"
-#include "reverse.hpp"
-#include "slice.hpp"
-#include "chain.hpp"
-#include "combinations_with_replacement.hpp"
-#include "combinations.hpp"
-#include "compress.hpp"
-#include "cycle.hpp"
-#include "dropwhile.hpp"
-#include "enumerate.hpp"
-#include "filter.hpp"
-#include "iterator_range.hpp"
-#include "permutations.hpp"
-#include "product.hpp"
-#include "range.hpp"
-#include "repeat.hpp"
-#include "reverse.hpp"
-#include "slice.hpp"
-#include "takewhile.hpp"
-#include "wrap_iter.hpp"
-#include "zip.hpp"
-#include "zip_longest.hpp"
-#include "powerset.hpp"
-#include "moving_section.hpp"
-#include "grouper.hpp"
-
+#include <chain.hpp>
+#include <combinations.hpp>
+#include <combinations_with_replacement.hpp>
+#include <compress.hpp>
+#include <count.hpp>
+#include <cycle.hpp>
+#include <dropwhile.hpp>
+#include <enumerate.hpp>
+#include <filter.hpp>
+#include <filterfalse.hpp>
+#include <groupby.hpp>
+#include <grouper.hpp>
+#include <imap.hpp>
+#include <iterator_range.hpp>
+#include <iterbase.hpp>
+#include <moving_section.hpp>
+#include <permutations.hpp>
+#include <powerset.hpp>
+#include <product.hpp>
+#include <range.hpp>
+#include <repeat.hpp>
+#include <reverse.hpp>
+#include <slice.hpp>
+#include <sorted.hpp>
+#include <takewhile.hpp>
+#include <unique_everseen.hpp>
+#include <unique_justseen.hpp>
+#include <wrap_iter.hpp>
+#include <zip.hpp>
+#include <zip_longest.hpp>
 //not sure if should include "iterator_range.hpp"
 //since it's already in everything
 
 #endif
+
 
