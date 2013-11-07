c3c909e92728a870670ee4ad838434ec415cee73
Jared Schmitz <jared@cs.columbia.edu>
2013-11-07 16:58:07 -0500

    Refactor slice

--- slice.hpp
+++ slice.hpp
@@ -15,37 +15,24 @@ namespace iter {
             DifferenceType step = 1
             ) -> iterator_range<wrap_iter<decltype(std::begin(container))>>
     {
-        //it seems like you can handle negative and positive ranges the same
-        //kept both checks to make checking for invalid slice more readable
-        if (begin > end && step < 0) {
-            DifferenceType new_end = end - ((end - begin) % step);
-            auto begin_iter = std::begin(container);
-            std::advance(begin_iter,begin);
-            auto end_iter = std::begin(container);
-            std::advance(end_iter,new_end);
+        // Check for an invalid slice. The sign of step must be equal to the
+        // sign of (end - begin). Since we don't force DifferenceType to be a
+        // primitive, just compare.
+        if (!(begin > end && step < 0) && !(begin <= end && step > 0)) {
+            // Just in case it gets dereferenced it will be the first element
+            // had the range been valid. This handles zero-length slices.
+            auto empty = std::next(std::begin(container), begin);
             return iterator_range<wrap_iter<decltype(std::begin(container))>>(
-                    make_wrap_iter(begin_iter,step),
-                    make_wrap_iter(end_iter,step));
+                    make_wrap_iter(empty, step),
+                    make_wrap_iter(empty, step));
         }
-        else if (begin <= end && step > 0) {
-            DifferenceType new_end = end - ((end - begin) % step);
-            auto begin_iter = std::begin(container);
-            std::advance(begin_iter,begin);
-            auto end_iter = std::begin(container);
-            std::advance(end_iter,new_end);
-            return iterator_range<wrap_iter<decltype(std::begin(container))>>(
-                    make_wrap_iter(begin_iter,step),
-                    make_wrap_iter(end_iter,step));
-        }
-        else {//return an empty range for invalid slice
-            auto empty = std::begin(container);
-            std::advance(empty,begin);
-            //just in case it gets dereferenced it will be the first element had 
-            //the range been valid
-            return iterator_range<wrap_iter<decltype(std::begin(container))>>(
-                    make_wrap_iter(empty,step),
-                    make_wrap_iter(empty,step));
-        }
+        // Return the iterator range
+        DifferenceType new_end = end - ((end - begin) % step);
+        auto begin_iter = std::next(std::begin(container), begin);
+        auto end_iter = std::next(std::begin(container), new_end);
+        return iterator_range<wrap_iter<decltype(std::begin(container))>>(
+                make_wrap_iter(begin_iter,step),
+                make_wrap_iter(end_iter,step));
 
     }
     //only give the end as an arg and assume step  is 1 and begin is 0
