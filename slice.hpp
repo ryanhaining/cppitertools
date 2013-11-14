d6e3ee29a21bf6dc9dfabba41f608d4b7243c850
Ryan Haining <rhainin1@binghamton.edu>
2013-11-13 23:33:21 -0500

    Starts converting slice.hpp to use helper Slice

--- slice.hpp
+++ slice.hpp
@@ -45,6 +45,87 @@ namespace iter {
         return slice(std::forward<Container>(container),0,end);
     }
 
+    //Forward declarations of Slice and slice
+    template <typename Container>
+    class Slice;
+
+    //template <typename T>
+    //Slice<std::initializer_list<T>> slice( std::initializer_list<T> &&);
+
+    template <typename Container>
+    Slice<Container> slice(Container &&);
+
+    template <typename Container>
+    class Slice : public IterBase<Container>{
+        private:
+            Container & container;
+
+            // The only thing allowed to directly instantiate an Slice is
+            // the slice function
+            friend Slice slice<Container>(Container &&);
+            //template <typename T>
+            //friend Slice<std::initializer_list<T>> slice(std::initializer_list<T> &&);
+
+            using typename IterBase<Container>::contained_iter_type;
+
+            using typename IterBase<Container>::contained_iter_ret;
+
+            Slice(Container & container) : container(container) { }
+            
+        public:
+            // Value constructor for use only in the slice function
+            Slice () = delete;
+            Slice & operator=(const Slice &) = delete;
+
+            Slice(const Slice &) = default;
+
+
+            //  Holds an iterator of the contained type and a size_t for the
+            //  index.  Each call to ++ increments both of these data members.
+            //  Each dereference returns an IterYield.
+            class Iterator {
+                private:
+                    contained_iter_type sub_iter;
+                public:
+                    Iterator (contained_iter_type si) :
+                        sub_iter(si)
+                    { }
+
+                    IterYield operator*() const {
+                        return IterYield(this->index, *this->sub_iter);
+                    }
+
+                    Iterator & operator++() { 
+                        ++this->sub_iter;
+                        return *this;
+                    }
+
+                    bool operator!=(const Iterator & other) const {
+                        return this->sub_iter != other.sub_iter;
+                    }
+            };
+
+            Iterator begin() const {
+                return Iterator(std::begin(this->container));
+            }
+
+            Iterator end() const {
+                return Iterator(std::end(this->container));
+            }
+
+    };
+    // Helper function to instantiate an Slice
+    template <typename Container>
+    Slice<Container> slice(Container && container) {
+        return Slice<Container>(std::forward<Container>(container));
+    }
+
+    template <typename T>
+    Slice<std::initializer_list<T>> slice(std::initializer_list<T> && il)
+    {
+        return Slice<std::initializer_list<T>>(il);
+    }
+
 }
 
 #endif //SLICE_HPP
