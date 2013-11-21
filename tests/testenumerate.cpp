62f2fb03514fbe7d87f2da0e8ae4457c81cf531e
Ryan Haining <rhainin1@binghamton.edu>
2013-11-21 13:22:33 -0500

    Adds test with range temporary
    for (auto i : enumerate(range(...))), something I've been meaning to do
    for a while.

--- tests/testenumerate.cpp
+++ tests/testenumerate.cpp
@@ -1,24 +1,28 @@
 #include <enumerate.hpp>
+#include <range.hpp>
 
 #include <string>
 #include <iostream>
 #include <vector>
 
 using iter::enumerate;
+using iter::range;
 
 int main() {
-    std::string s = "hello world";
-
+    std::cout << "const std::string\n";
     const std::string const_string("goodbye world");
     for (auto e : enumerate(const_string)) {
         std::cout << e.index << ": " << e.element << std::endl;
     }
+
 
     std::vector<int> vec;
     for(int i = 0; i < 12; ++i) { 
         vec.push_back(i * i);
     }
 
+
+    std::cout << "print vector element, set it to zero, then print it again\n";
     for (auto e : enumerate(vec)) { 
         std::cout << e.index << ": " << e.element << std::endl;
         e.element = 0;
@@ -26,14 +30,22 @@ int main() {
         std::cout << e.index << ": " << e.element << std::endl;
     }
 
+    std::cout << "static array\n";
     int array[] = {1, 9, 8, 11};
     for (auto e : enumerate(array)) {
         std::cout << e.index << ": " << e.element << '\n';
     }
 
 
+    std::cout << "initializer list\n";
     for (auto e : enumerate({0, 1, 4, 9, 16, 25})) {
         std::cout << e.index << "^2 = " << e.element << '\n';
+    }
+
+
+    std::cout << "range(10, 20, 2)\n";
+    for (auto e : enumerate(range(10, 20, 2))) {
+        std::cout << e.index << ": " << e.element << '\n';
     }
 
     return 0;
