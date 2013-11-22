ee77cd61c55cf1d3a67ab457c508056f39349c7b
Ryan Haining <rhainin1@binghamton.edu>
2013-11-21 23:52:52 -0500

    Adds test with temporary

--- tests/testdropwhile.cpp
+++ tests/testdropwhile.cpp
@@ -1,13 +1,19 @@
 #include <dropwhile.hpp>
+#include <range.hpp>
 
 #include <vector>
 #include <iostream>
 
 using iter::dropwhile;
+using iter::range;
 
 int main() {
     std::vector<int> ivec{1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4};
     for (auto i : dropwhile([] (int i) {return i < 5;}, ivec)) {
+        std::cout << i << '\n';
+    }
+
+    for (auto i : dropwhile([] (int i) {return i < 5;}, range(10))) {
         std::cout << i << '\n';
     }
     return 0;
