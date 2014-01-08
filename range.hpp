4d797e34e8b7b42d97d2c394ba6a7fbe916933e2
Ben Jones <ben.james.jones@gmail.com>
2014-01-08 15:34:17 -0700

    added specialization of range w/ unsigned types

--- range.hpp
+++ range.hpp
@@ -94,10 +94,17 @@ namespace iter {
                     // 2) (stop - start) % step != 0.  For
                     // example Range(1, 10, 2).  The iterator will never be
                     // exactly equal to the stop value.
-                    bool operator!=(const Range::Iterator & other) const {
-                        return !(this->step > 0 && this->value >= other.value) 
-                            && !(this->step < 0 && this->value <= other.value);
-                    }
+                    bool operator!=(const Range::Iterator & other) const { 
+				         return not_equal_to(other, typename std::is_unsigned<T>::type());
+			        }
+			  private:
+			        bool not_equal_to(const Range::Iterator& other, std::true_type /*unsigned*/) const{
+					     return this->value < other.value;
+			        }
+			        bool not_equal_to(const Range::Iterator& other, std::false_type /*signed*/) const{
+					     return !(this->step > 0 && this->value >= other.value) 
+						   && !(this->step < 0 && this->value <= other.value);
+			        }
             };
 
             Iterator begin() const {
