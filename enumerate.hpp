cd88f0e60a04da1cdba4b4fbac2afe6e4b430a34
Ryan Haining <rhainin1@binghamton.edu>
2014-05-28 23:18:28 -0700

    Safe support for temporaries in enumerate
    I will need to add this to everything soon.  This works by adding an
    overload to the enumerate() function.  If an lvalue is passed, the
    Enumerable binds an lvalue to it.  If an rvalue is passed, the
    Enumerable move constructs it.  It's easier explained in the code.
    
    Also, must make the value constructor private again to complete this

--- enumerate.hpp
+++ enumerate.hpp
@@ -20,6 +20,7 @@
 
 namespace iter {
 
+#if 0
     //Forward declarations of Enumerable and enumerate
     template <typename Container>
     class Enumerable;
@@ -29,27 +30,37 @@ namespace iter {
 
     template <typename Container>
     Enumerable<Container> enumerate(Container&&);
+#endif
 
     template <typename Container>
     class Enumerable {
-        private:
-            Container& container;
+        //private:
+        public:
+            Container container;
 
+#if 0
             // The only thing allowed to directly instantiate an Enumerable is
             // the enumerate function
-            friend Enumerable enumerate<Container>(Container&&);
+            friend Enumerable enumerate<Container>(Container);
             template <typename T>
             friend Enumerable<std::initializer_list<T>> enumerate(
                     std::initializer_list<T>&&);
-
-            Enumerable(Container&& container) : container{container} { }
+#endif
+    
+            Enumerable(Container container)
+                : container(std::forward<Container>(container))
+            { }
             
         public:
             // Value constructor for use only in the enumerate function
-            Enumerable () = delete;
-            Enumerable & operator=(const Enumerable&) = delete;
+            Enumerable() = delete;
+            Enumerable& operator=(const Enumerable&) = delete;
+            Enumerable(const Enumerable&) = delete;
+            Enumerable& operator=(Enumerable&&) = delete;
 
-            Enumerable(const Enumerable&) = default;
+            // movable only
+            Enumerable(Enumerable&&) = default;
+            ~Enumerable() = default;
 
             // "yielded" by the Enumerable::Iterator.  Has a .index, and a 
             // .element referencing the value yielded by the subiterator
@@ -91,27 +102,38 @@ namespace iter {
                     }
             };
 
-            Iterator begin() const {
+            Iterator begin() {
                 return {std::begin(this->container)};
             }
 
-            Iterator end() const {
+            Iterator end() {
                 return {std::end(this->container)};
             }
 
     };
 
-    // Helper function to instantiate an Enumerable
+    // any lvalues passed will go here, instantiating Enumerabe<Container&>
+    // this will result in enumerate iterating over a reference to the
+    // provided value, as is the usual intention
     template <typename Container>
-    Enumerable<Container> enumerate(Container&& container) {
-        return {std::forward<Container>(container)};
+    Enumerable<Container&> enumerate(Container& container) {
+        return {container};
     }
 
+    // any rvalue passed will go here, instantiating Enumerabel<Container>
+    // instead of binding a reference, the Enumerable ctor will move construct
+    // the temporary `container` as a data member
+    template <typename Container>
+    Enumerable<Container> enumerate(Container&& container) {
+        return {std::move(container)};
+    }
+
+    // for initializer lists.  copy constructs the list into the Enumerable
     template <typename T>
     Enumerable<std::initializer_list<T>> enumerate(
-            std::initializer_list<T>&& il)
+            std::initializer_list<T> il)
     {
-        return {std::move(il)};
+        return {il};
     }
 
 }
