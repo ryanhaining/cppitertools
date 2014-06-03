0e82aa0272be2c22be6f9ba3c6e33775c1890453
Ryan Haining <rhainin1@binghamton.edu>
2014-06-02 22:38:01 -0700

    Adds reversed test with temporary

--- tests/testreversed.cpp
+++ tests/testreversed.cpp
@@ -6,27 +6,32 @@
 #include <string>
 
 int main () {
-    {
-        std::vector<int> a{1,2,3,4,5,6,7};
-        std::vector<std::string> b{"hey","how","are","you","doing"};
-        std::cout << std::endl << "reversed range test" << std::endl << std::endl;
-        for (auto i : iter::reversed(a)) {
-            std::cout << i << std::endl;
-        }
-        std::cout<<std::endl;
-        //modify a
-        for (auto & i : iter::reversed(a)) {
-            std::cout << i << std::endl;
-            i = 9;
-        }
-        std::cout<<std::endl;
-        for (auto i : iter::reversed(a)) {
-            std::cout << i << std::endl;
-        }
-        std::cout<<std::endl;
-        //try strings
-        for (auto s : iter::reversed(b)) {
-            std::cout << s << std::endl;
-        }
+    std::vector<int> a{1,2,3,4,5,6,7};
+    std::vector<std::string> b{"hey","how","are","you","doing"};
+    std::cout << std::endl << "reversed range test" << std::endl << std::endl;
+    for (auto i : iter::reversed(a)) {
+        std::cout << i << std::endl;
     }
+    std::cout<<std::endl;
+    //modify a
+    for (auto & i : iter::reversed(a)) {
+        std::cout << i << std::endl;
+        i = 9;
+    }
+    std::cout<<std::endl;
+    for (auto i : iter::reversed(a)) {
+        std::cout << i << std::endl;
+    }
+    std::cout<<std::endl;
+    //try strings
+    for (auto s : iter::reversed(b)) {
+        std::cout << s << std::endl;
+    }
+
+    std::cout << "with temporary\n";
+    for (auto i : iter::reversed(std::vector<int>{1, 2, 3, 4, 5, 6, 7})) {
+        std::cout << i << std::endl;
+    }
+
+
 }
