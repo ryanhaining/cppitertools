773d434b116a07daa18b7b1c0ec0a7ad11a2f220
Ryan Haining <rhainin1@binghamton.edu>
2014-06-07 23:29:05 -0700

    adds tests for movable temporaries

--- tests/testrepeat.cpp
+++ tests/testrepeat.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <array>
 #include <string>
+#include <memory>
 
 int main () {
     int a = 10;
@@ -16,4 +17,15 @@ int main () {
     for (auto num : iter::repeat(a,10)) {//goes ten times
         std::cout << num << std::endl;
     }
+    
+    std::cout << "with temporary\n";
+    for (auto s : iter::repeat(std::string{"hey"}, 2)) {
+        std::cout <<  s << '\n';
+    }
+
+    std::cout << "with uptr temporary\n";
+    for (auto& p : iter::repeat(std::unique_ptr<int>{new int{2}}, 2)) {
+        std::cout << *p << '\n';
+    }
+
 }
