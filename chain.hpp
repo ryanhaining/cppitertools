72ce2ceeb46e94032773eb8ad4115f2d3240e7bf
Ryan Haining <rhainin1@binghamton.edu>
2014-06-08 15:00:33 -0700

    Adds chain.from_iterable
    Works with an iterable of iterables, rather than a variadic number of
    iterables

--- chain.hpp
+++ chain.hpp
@@ -1,7 +1,11 @@
+#ifndef CHAIN__HPP__
+#define CHAIN__HPP__
+
 #include "iterbase.hpp"
 
 #include <utility>
 #include <iterator>
+#include <memory>
 #include <initializer_list>
 #include <type_traits>
 
@@ -129,12 +133,88 @@ namespace iter {
             }
     };
 
+    template <typename Container>
+    class ChainedFromIterable {
+        private:
+            Container container;
+            friend class ChainMaker;
+            ChainedFromIterable(Container container)
+                : container(std::forward<Container>(container))
+            { }
+
+        public:
+            class Iterator {
+                private:
+                    using SubContainer = iterator_deref<Container>;
+                    using SubIter = iterator_type<SubContainer>;
+
+                   iterator_type<Container> top_level_iter;
+                   const iterator_type<Container> top_level_end;
+                   std::unique_ptr<SubIter> sub_iter_p;
+                   std::unique_ptr<SubIter> sub_end_p;
+                public:
+                   Iterator(iterator_type<Container> top_iter,
+                           iterator_type<Container> top_end)
+                       : top_level_iter{top_iter},
+                       top_level_end{top_end},
+                       sub_iter_p{!(top_iter != top_end) ?  // iter == end ?
+                           nullptr : new SubIter{std::begin(*top_iter)}},
+                       sub_end_p{!(top_iter != top_end) ?  // iter == end ?
+                           nullptr : new SubIter{std::end(*top_iter)}}
+                   { }
+
+                   Iterator& operator++() {
+                       ++*this->sub_iter_p;
+                       if (!(*this->sub_iter_p != *this->sub_end_p)) {
+                           ++this->top_level_iter;
+                           if (this->top_level_iter != this->top_level_end) {
+                               sub_iter_p.reset(
+                                       new SubIter{std::begin(*this->top_level_iter)});
+                               sub_end_p.reset(
+                                       new SubIter{std::end(*this->top_level_iter)});
+                           } else {
+                               sub_iter_p.reset(nullptr);
+                               sub_end_p.reset(nullptr);
+                           }
+                       }
+                       return *this;
+                   }
+                       
+
+                   bool operator!=(const Iterator& other) const {
+                       return this->top_level_iter != other.top_level_iter &&
+                           (this->sub_iter_p != other.sub_iter_p ||
+                            *this->sub_iter_p != *other.sub_iter_p);
+                   }
+
+                   iterator_deref<iterator_deref<Container>> operator*() {
+                       return **this->sub_iter_p;
+                   }
+           };
+
+           Iterator begin() {
+               return {std::begin(this->container), std::end(this->container)};
+           }
+
+           Iterator end() {
+               return {std::end(this->container), std::end(this->container)};
+           }
+    };
+
+
     class ChainMaker {
         public:
             // expose regular call operator to provide usual chain()
             template <typename... Containers>
             Chained<Containers...> operator()(Containers&&... cs) const {
                 return {std::forward<Containers>(cs)...};
+            }
+
+            // chain.from_iterable
+            template <typename Container>
+            ChainedFromIterable<Container> from_iterable(
+                    Container&& container) const {
+                return {std::forward<Container>(container)};
             }
     };
 
@@ -143,3 +223,5 @@ namespace iter {
     }
 
 }
+
+#endif //#define CHAIN__HPP__
