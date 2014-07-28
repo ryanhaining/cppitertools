7d9cae1a94e40571cc4773e9c994e0dc82659006
Ryan Haining <rhainin1@binghamton.edu>
2014-07-27 23:32:21 -0700

    tests combinations with temporary and static array

--- tests/testcombinations.cpp
+++ tests/testcombinations.cpp
@@ -25,9 +25,24 @@ int main() {
         for (auto j : i ) std::cout << j << " ";
         std::cout<<std::endl;
     }
+    std::cout << "initializer list\n";
     for (auto i : combinations({1,2,3,4},3)) {
         for (auto j : i ) std::cout << j << " ";
         std::cout<<std::endl;
     }
+
+    std::cout << "temporary\n";
+    for (auto i : combinations(std::vector<int>{1,2,3,4,5}, 3)) {
+        for (auto j : i ) std::cout << j << " ";
+        std::cout<<std::endl;
+    }
+
+    std::cout << "static array\n";
+    int array[] = {1,2,3,4,5};
+    for (auto i : combinations(array, 3)) {
+        for (auto j : i) std::cout << j << " ";
+        std::cout << '\n';
+    }
+
     return 0;
 }
