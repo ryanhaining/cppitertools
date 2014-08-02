f83d42846a84a9839e66ce5bb2cf97ac20e030db
Ryan Haining <rhainin1@binghamton.edu>
2014-08-02 14:56:58 -0700

    adds support for temporaries

--- combinations_with_replacement.hpp
+++ combinations_with_replacement.hpp
@@ -2,8 +2,8 @@
 #define COMBINATIONS_WITH_REPLACEMENT_HPP
 
 #include "iterator_range.hpp"
+#include "iterbase.hpp"
 
-#include <array>
 #include <vector>
 #include <type_traits>
 
@@ -13,12 +13,13 @@ namespace iter {
     //of items in your combination at runtime, but rather it is a way to view 
     //a list based on the problem your solving, that being said it would be easy
     //to make it decided at runtime
+#if 0
     template <typename Container>
         struct combinations_with_replacement_iter;
 
     template <typename Container>
         iterator_range<combinations_with_replacement_iter<Container>>
-        combinations_with_replacement(const Container & container, size_t N) {
+        combinations_with_replacement(Container & container, std::size_t N) {
             auto begin = combinations_with_replacement_iter<Container>(container, N);
             auto end = combinations_with_replacement_iter<Container>(container, N);
             return 
@@ -26,82 +27,99 @@ namespace iter {
         }
      template <typename T>
         iterator_range<combinations_with_replacement_iter<std::initializer_list<T>>>
-        combinations_with_replacement(std::initializer_list<T> && container, size_t N) {
+        combinations_with_replacement(std::initializer_list<T> && container, std::size_t N) {
             auto begin = combinations_with_replacement_iter<std::initializer_list<T>>(container, N);
             auto end = combinations_with_replacement_iter<std::initializer_list<T>>(container, N);
             return {begin,end};
         }
-   template <typename Container>
-        struct combinations_with_replacement_iter
-        {
+#endif
+    template <typename Container>
+    class CombinatorWithReplacement {
         private:
-            const Container & items;
-            std::vector<decltype(std::begin(items))> indicies;
-            bool not_done = true;
+            Container container;
+            std::size_t length;
 
         public:
-            //Holy shit look at this typedef
-            using item_t = typename 
-                std::remove_const<
-                typename std::remove_reference<decltype(*(std::begin(items)))>::type>::type;
-            combinations_with_replacement_iter(const Container & i, size_t N) : 
-                items(i), indicies(N)
-            {
-                if (N == 0) {
-                    not_done = false;
-                    return;
-                }
-                for (auto & iter : indicies) iter = std::begin(items);
-            }
-            //technically should be a dynarray
-            std::vector<item_t> operator*()const
-            {
-                std::vector<item_t> values;
-                for (auto i : indicies) {
-                    values.push_back(*i);
-                }
-                return values;
-            }
+           CombinatorWithReplacement(Container container, std::size_t n)
+               : container(std::forward<Container>(container)),
+               length{n}
+           { }
+
+           class Iterator {
+               private:
+                   Container& items;
+                   std::vector<iterator_type<Container>> indicies;
+                   bool not_done;
+
+               public:
+                   using item_t =
+                       typename std::remove_const<
+                           typename std::remove_reference<
+                               iterator_deref<Container>>::type>::type;
+
+                   Iterator(
+                           Container& container, std::size_t n)
+                       : items(container),
+                       indicies(n, std::begin(items)),
+                       not_done{n != 0}
+                   { }
+
+                   std::vector<item_t> operator*()
+                   {
+                       std::vector<item_t> values;
+                       for (auto i : indicies) {
+                           values.push_back(*i);
+                       }
+                       return values;
+                   }
 
 
-            combinations_with_replacement_iter &
-            operator++() 
-            {
-                for (auto iter = indicies.rbegin(); iter != indicies.rend(); ++iter) {
-                    ++(*iter);
-                    if (*iter == std::end(items)) {
-                        if ( (iter + 1) != indicies.rend()) {
-                            for (auto down = iter; down != indicies.rbegin()-1;--down) {
-                                (*down) = (*(iter + 1)) + 1; 
-                            }
-                        }
-                        else {
-                            not_done = false;
-                            break;
-                        }
-                    }
-                    else break; 
-                    //we break because none of the rest of the items need to 
-                    //be incremented
-                }
-                return *this;
-            }
+                   Iterator& operator++() {
+                       for (auto iter = indicies.rbegin(); iter != indicies.rend(); ++iter) {
+                           ++(*iter);
+                           if (*iter == std::end(items)) {
+                               if ( (iter + 1) != indicies.rend()) {
+                                   for (auto down = iter; down != indicies.rbegin()-1;--down) {
+                                       (*down) = (*(iter + 1)) + 1; 
+                                   }
+                               }
+                               else {
+                                   not_done = false;
+                                   break;
+                               }
+                           }
+                           else break; 
+                           //we break because none of the rest of the items need to 
+                           //be incremented
+                       }
+                       return *this;
+                   }
 
-            bool operator !=(const combinations_with_replacement_iter &)
-            {
-                //because of the way this is done you have to start from the 
-                //begining of the range and end at the end, you could break in 
-                //the middle of the loop though, it's not different from the way
-                //that python's works
-                return not_done;
-            }
-        };
-}
-namespace std {
+                   bool operator !=(const Iterator&) const
+                   {
+                       //because of the way this is done you have to start from the 
+                       //begining of the range and end at the end, you could break in 
+                       //the middle of the loop though, it's not different from the way
+                       //that python's works
+                       return not_done;
+                   }
+           };
+
+           Iterator begin() {
+               return {this->container, this->length};
+           }
+
+           Iterator end() {
+               return {this->container, 0};
+           }
+    };
+
     template <typename Container>
-        struct iterator_traits<iter::combinations_with_replacement_iter<Container>> {
-            using difference_type = ptrdiff_t;
-            using iterator_category = input_iterator_tag;
-        };
+    CombinatorWithReplacement<Container> combinations_with_replacement(
+            Container&& container, std::size_t length) {
+        return {std::forward<Container>(container), length};
+    }
+
 }
+
 #endif //COMBINATIONS_WITH_REPLACEMENT_HPP
