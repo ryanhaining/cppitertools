fadb026defc24dcfebece8895c2ab350c0609fa3
Ryan Haining <rhainin1@binghamton.edu>
2014-08-03 16:42:50 -0700

    adds test with init list

--- tests/testunique_justseen.cpp
+++ tests/testunique_justseen.cpp
@@ -9,6 +9,20 @@ int main() {
     std::vector<int> v {1,1,1,2,2,4,4,5,6,7,8,8,8,8,9,9};
     for (auto i : unique_justseen(v)) {
         std::cout << i << " ";
-    }std::cout << std::endl;
+    }
+    std::cout << '\n';
+
+    std::cout << "with temporary\n";
+    for (auto i : unique_justseen({1,1,1,2,3,3})) {
+        std::cout << i << " ";
+    }
+    std::cout << '\n';
+
+    std::cout << "with init list\n";
+    for (auto i : unique_justseen({1,1,1,2,3,3})) {
+        std::cout << i << " ";
+    }
+    std::cout << '\n';
+
     return 0;
 }
