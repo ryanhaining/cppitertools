97c27ecb99245145a87f4fd8eae7252ec4a72953
Ryan Haining <rhainin1@binghamton.edu>
2014-08-02 22:04:35 -0700

    adds test with init list

--- tests/testsliding_window.cpp
+++ tests/testsliding_window.cpp
@@ -12,10 +12,17 @@ int main() {
         std::cout << std::endl;
     }
 
-    for (auto sec : sliding_window(std::vector<int>{1,2,3,4,5,6,7,8,9} ,4)) {
+    for (auto sec : sliding_window(std::vector<int>{1,2,3,4,5,6,7,8,9}, 4)) {
         for (auto i : sec) {
             std::cout << i << " ";
             i.get() = 90;
+        }
+        std::cout << std::endl;
+    }
+
+    for (auto sec : sliding_window({1,2,3,4,5,6,7,8,9}, 4)) {
+        for (auto i : sec) {
+            std::cout << i << " ";
         }
         std::cout << std::endl;
     }
