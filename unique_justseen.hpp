fba04b2b7497b1f7eef7dbe753d008fceddbd2e7
Ryan Haining <rhainin1@binghamton.edu>
2014-08-03 16:40:57 -0700

    shortens and supports temporaries

--- unique_justseen.hpp
+++ unique_justseen.hpp
@@ -1,7 +1,8 @@
 #ifndef UNIQUE_JUSTSEEN_HPP
 #define UNIQUE_JUSTSEEN_HPP
 
-#include "filter.hpp"
+#include "groupby.hpp"
+#include "imap.hpp"
 
 #include <type_traits>
 #include <functional>
@@ -9,24 +10,17 @@
 
 namespace iter 
 {
-    //this should be self evident but unique_justseen places the requirement 
-    //on the elements in the container have the != operator overloaded
+    // gets first of each group.  since each group is decided based on equality
+    // with the previous item, this results in each item only appearing once
     template <typename Container>
-    auto unique_justseen(Container && container) 
-    -> Filter<std::function<bool(decltype(container.front()))>,Container>
-    {
-        using elem_t = decltype(container.front());
-        auto last = container.begin();
-        std::function<bool(elem_t)> func = [last,container] (elem_t e) mutable 
-                {
-                    if (last == container.begin()) {
-                        return true;
-                    }
-                    else { 
-                        return *(++last) != e;
-                    }
-                };
-        return filter(func,std::forward<Container>(container));
+    auto unique_justseen(Container&& container) {
+        return imap(
+                [] (iterator_deref<
+                        decltype(
+                            groupby(
+                                std::forward<Container>(container)))>&& gb)
+                    {return *std::begin(gb.second);},
+                groupby(std::forward<Container>(container)));
     }
 }
 
