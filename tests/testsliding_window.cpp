37b502b2ac2e1bd9fbd74d241988fb85967059b6
Ryan Haining <rhainin1@binghamton.edu>
2014-08-02 22:24:42 -0700

    adds test with static array

--- tests/testsliding_window.cpp
+++ tests/testsliding_window.cpp
@@ -15,6 +15,7 @@ int main() {
         std::cout << std::endl;
     }
 
+    std::cout << "with temporary\n";
     for (auto sec : sliding_window(std::vector<int>{1,2,3,4,5,6,7,8,9}, 4)) {
         for (auto i : sec) {
             std::cout << i << " ";
@@ -23,6 +24,7 @@ int main() {
         std::cout << std::endl;
     }
 
+    std::cout << "with init list\n";
     for (auto sec : sliding_window({1,2,3,4,5,6,7,8,9}, 4)) {
         for (auto i : sec) {
             std::cout << i << " ";
@@ -30,7 +32,17 @@ int main() {
         std::cout << std::endl;
     }
 
+    std::cout << "with window_size > length\n";
     for (auto sec : sliding_window({1,2,3}, 10)) {
+        for (auto i : sec) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "with static array\n";
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    for (auto sec : sliding_window(arr, 4)) {
         for (auto i : sec) {
             std::cout << i << " ";
         }
