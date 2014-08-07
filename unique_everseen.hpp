a50172d61f14ac633828bdfe7ded9354939234e6
Ryan Haining <rhainin1@binghamton.edu>
2014-08-06 22:01:26 -0700

    replaces unordered_map with unordered_set

--- unique_everseen.hpp
+++ unique_everseen.hpp
@@ -6,27 +6,29 @@
 #include <type_traits>
 #include <functional>
 #include <utility>
-#include <unordered_map>
+#include <unordered_set>
+#include <iterator>
 
 namespace iter 
 {
     //the container type must be usable in an unordered_map to achieve constant
     //performance checking if it has ever been seen
     template <typename Container>
-    auto unique_everseen(Container && container) 
-    -> Filter<std::function<bool(decltype(container.front()))>,Container>
+    auto unique_everseen(Container&& container) 
+        -> Filter<std::function<bool(decltype(container.front()))>,Container>
     {
         using elem_t = decltype(container.front());
-        std::unordered_map<typename std::remove_reference<elem_t>::type,bool> elem_seen;
+        std::unordered_set<typename std::remove_reference<elem_t>::type> elem_seen;
         std::function<bool(elem_t)> func = [elem_seen](elem_t e) mutable
         //has to be captured by value because it goes out of scope when the 
         //function returns
                 {
-                    if(!elem_seen[e]) {
-                        elem_seen[e] = true;
+                    if (elem_seen.find(e) == std::end(elem_seen)){
+                        elem_seen.insert(e);
                         return true;
+                    } else {
+                        return false;
                     }
-                    else return false;
                 };
         return filter(func,std::forward<Container>(container));
     }
