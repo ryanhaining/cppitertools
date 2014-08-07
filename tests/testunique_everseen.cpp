e0ef7527ed844853ea7c96ffbf059da75705c724
Ryan Haining <rhainin1@binghamton.edu>
2014-08-06 22:01:50 -0700

    adds test with temporary

--- tests/testunique_everseen.cpp
+++ tests/testunique_everseen.cpp
@@ -19,5 +19,11 @@ int main() {
             std::cout << i << " ";
         }std::cout << std::endl;
     }
+
+    for (auto i : unique_everseen(
+                std::vector<int>{1,2,1,3,4,3,2,1,5,6,7,7,8,9,8,9,6})) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
