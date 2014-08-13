9236beb24b0ca488c253c735230548e69c84cdf0
Ryan Haining <rhainin1@binghamton.edu>
2014-08-13 02:05:56 -0700

    ziplongest tests look a lot nicer

--- tests/testzip_longest.cpp
+++ tests/testzip_longest.cpp
@@ -9,14 +9,13 @@
 using iter::zip_longest;
 
 template <typename T>
-std::ostream & operator<<(std::ostream & o, const boost::optional<T> & opt) {
+std::ostream & operator<<(std::ostream & out, const boost::optional<T>& opt) {
     if (opt) {
-        std::cout << *opt << std::endl;
+        out << "Just " << *opt;
+    } else {
+        out << "Nothing";
     }
-    else {
-        std::cout << "Object disengaged of type " << typeid(T).name() << std::endl;
-    }
-    return o;
+    return out;
 }
 
 int main() {
@@ -44,39 +43,39 @@ int main() {
         std::array<double,5> d{{1.2,1.2,1.2,1.2,1.2}};
         std::cout << std::endl << "Variadic template zip_longest" << std::endl;
         for (auto e : iter::zip_longest(i,f,s,d)) {
-            std::cout << std::get<0>(e)
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
             *std::get<1>(e)=2.2f; //modify the float array
         }
         std::cout<<"modified array" <<std::endl;
         for (auto e : iter::zip_longest(i,s,f,d)) {
-            std::cout << std::get<0>(e) 
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
         }
         std::cout << std::endl << "Try some weird range differences" << std::endl;
         std::vector<int> empty{};
         for (auto e : iter::zip_longest(empty,f,s,d)) {
-            std::cout << std::get<0>(e) 
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
         for (auto e : iter::zip_longest(f,empty,s,d)) {
-            std::cout << std::get<0>(e) 
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
         for (auto e : iter::zip_longest(f,s,i,d)) { 
-            std::cout << std::get<0>(e) 
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
@@ -84,9 +83,9 @@ int main() {
                     std::initializer_list<double>{1.1,2.2,3.3,4.4},
                     std::initializer_list<double>{1.1,2.2,3.3,4.4},
                     std::array<int,3>{{1,2,3}})) { 
-            std::cout << std::get<0>(e) 
-                << std::get<1>(e) 
-                << std::get<2>(e) 
+            std::cout << std::get<0>(e) << ' '
+                << std::get<1>(e) << ' '
+                << std::get<2>(e) << ' '
                 << std::get<3>(e) << std::endl;
         }
         std::cout<<std::endl;
