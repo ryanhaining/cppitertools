4dc6b207ceb53ff9a2696d2686e751e75a21b34c
Ryan Haining <rhainin1@binghamton.edu>
2014-09-08 22:14:28 -0400

    make distances default to 1 (correctly)

--- iterbase.hpp
+++ iterbase.hpp
@@ -15,34 +15,33 @@
 namespace iter {
     // because std::advance assumes a lot and is actually smart, I need a dumb
     // version that will work with most things
-    template <typename InputIt, typename Distance>
-    void dumb_advance(InputIt& iter, Distance distance) {
+    template <typename InputIt, typename Distance =std::size_t>
+    void dumb_advance(InputIt& iter, Distance distance=1) {
         for (Distance i(0); i < distance; ++i) {
             ++iter;
         }
     }
 
     // iter will not be incremented past end
-    template <typename InputIt, typename Distance>
+    template <typename InputIt, typename Distance =std::size_t>
     void dumb_advance(InputIt& iter, const InputIt& end, Distance distance=1) {
         for (Distance i(0); i < distance && iter != end; ++i) {
             ++iter;
         }
     }
 
-    template <typename ForwardIt, typename Distance>
-    ForwardIt dumb_next(ForwardIt it, Distance distance) {
+    template <typename ForwardIt, typename Distance =std::size_t>
+    ForwardIt dumb_next(ForwardIt it, Distance distance=1) {
         dumb_advance(it, distance);
         return it;
     }
 
-    template <typename ForwardIt, typename Distance>
+    template <typename ForwardIt, typename Distance =std::size_t>
     ForwardIt dumb_next(
             ForwardIt it, const ForwardIt& end, Distance distance=1) {
         dumb_advance(it, end, distance);
         return it;
     }
-
 
     // iterator_type<C> is the type of C's iterator
     template <typename Container>
@@ -65,6 +64,20 @@ namespace iter {
     template <typename Container>
     using reverse_iterator_deref = 
         decltype(*std::declval<reverse_iterator_type<Container>&>());
+
+    // For combinatoric functions, if the Containers iterator dereferences
+    // to a reference, then this is a std::reference_wrapper for that type
+    // otherwise it's a non-const of that type
+    template <typename Container>
+    using collection_item_type =
+        typename std::conditional<
+        std::is_reference<iterator_deref<Container>>::value,
+        std::reference_wrapper<
+            typename std::remove_reference<
+            iterator_deref<Container>>::type>,
+        typename std::remove_const<
+            iterator_deref<Container>>::type>::type;
+
 }
 
 #endif // #ifndef ITERBASE_HPP_
