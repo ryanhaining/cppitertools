84d382bda543af17f30371ca53cc5a3730dace8e
Ryan Haining <rhainin1@binghamton.edu>
2014-09-28 20:41:07 -0400

    Flattens zip implementation
    No recursive template. Using std::index_sequence everywhere now.

--- zip.hpp
+++ zip.hpp
@@ -6,9 +6,14 @@
 #include <iterator>
 #include <tuple>
 #include <utility>
+#include <algorithm>
 
 
 namespace iter {
+
+    template <typename... Ts>
+    void absorb(Ts&&...) { }
+
     template <typename... RestContainers>
     class Zipped;
 
@@ -16,114 +21,88 @@ namespace iter {
     Zipped<Containers...> zip(Containers&&...);
 
     // specialization for at least 1 template argument
-    template <typename Container, typename... RestContainers>
-    class Zipped <Container, RestContainers...> {
-        static_assert(!std::is_rvalue_reference<Container>::value,
-                "Itertools cannot be templated with rvalue references");
-
-        friend Zipped zip<Container, RestContainers...>(
-                Container&&, RestContainers&&...);
-
-        template <typename... RC>
-        friend class Zipped;
-
+    template <typename... Containers>
+    class Zipped {
         private:
-            Container container;
-            Zipped<RestContainers...> rest_zipped;
-            Zipped(Container container, RestContainers&&... rest)
-                : container(std::forward<Container>(container)),
-                rest_zipped{std::forward<RestContainers>(rest)...}
+            std::tuple<Containers...> containers;
+            std::make_index_sequence<sizeof...(Containers)> indices;
+            
+        public:
+            Zipped(Containers... in_containers)
+                : containers{std::forward<Containers>(in_containers)...}
             { }
 
-        public:
             class Iterator {
                 private:
-                    using RestIter =
-                        typename Zipped<RestContainers...>::Iterator;
+                    std::tuple<iterator_type<Containers>...> iters;
+                    std::make_index_sequence<sizeof...(Containers)> indices;
 
-                    iterator_type<Container> iter;
-                    RestIter rest_iter;
+                    template <std::size_t... Is>
+                    bool not_equal(const Iterator& other,
+                                   std::index_sequence<Is...>) const {
+                        if (sizeof...(Is) == 0) {
+                            // empty zip() case, return false right away
+                            return false;
+                        }
+                        bool results[] = { true,
+                            (std::get<Is>(this->iters) !=
+                             std::get<Is>(other.iters))...
+                        };
+                        return std::all_of(
+                                std::begin(results), std::end(results),
+                                [](bool b){ return b; } );
+                    }
+
+                    template <std::size_t... Is>
+                    void increment(std::index_sequence<Is...>) {
+                        absorb(++std::get<Is>(this->iters)...);
+                    }
+
+                    template <std::size_t... Is>
+                    auto deref(std::index_sequence<Is...>) {
+                        return std::tuple<iterator_deref<Containers>...>{
+                            (*std::get<Is>(this->iters))...};
+                    }
+
                 public:
-                    constexpr static const bool is_base_iter = false;
-                    Iterator(iterator_type<Container> it, const RestIter& rest)
-                        : iter{it},
-                        rest_iter{rest}
+                    Iterator(iterator_type<Containers>... its)
+                        : iters{its...}
                     { }
 
                     Iterator& operator++() {
-                        ++this->iter;
-                        ++this->rest_iter;
+                        this->increment(this->indices);
                         return *this;
                     }
 
                     bool operator!=(const Iterator& other) const {
-                        return this->iter != other.iter &&
-                            (RestIter::is_base_iter ||
-                                this->rest_iter != other.rest_iter);
+                        return this->not_equal(other, this->indices);
                     }
 
-                    auto operator*() ->
-                        decltype(std::tuple_cat(
-                                    std::tuple<iterator_deref<Container>>{
-                                        *this->iter},
-                                    *this->rest_iter))
-                    {
-                        return std::tuple_cat(
-                                std::tuple<iterator_deref<Container>>{
-                                    *this->iter},
-                                *this->rest_iter);
+                    auto operator*() {
+                        return this->deref(this->indices);
                     }
             };
-
-            Iterator begin() {
-                return {std::begin(this->container),
-                    std::begin(this->rest_zipped)};
+        private:
+            template <std::size_t... Is>
+            Iterator make_begin(std::index_sequence<Is...>) {
+                return {std::begin(std::get<Is>(this->containers))...};
             }
 
-            Iterator end() {
-                return {std::end(this->container),
-                    std::end(this->rest_zipped)};
+            template <std::size_t... Is>
+            Iterator make_end(std::index_sequence<Is...>) {
+                return {std::end(std::get<Is>(this->containers))...};
             }
-    };
 
-
-    template <>
-    class Zipped<> {
         public:
-            class Iterator {
-                public:
-                    constexpr static const bool is_base_iter = true;
-
-                    Iterator() { }
-                    Iterator(const Iterator&) { }
-                    Iterator& operator=(const Iterator&) { return *this; }
-
-                    Iterator& operator++() {
-                        return *this;
-                    }
-
-                    // if this were to return true, there would be no need
-                    // for the is_base_iter static class attribute.
-                    // However, returning false causes an empty zip() call
-                    // to reach the "end" immediately. Returning true here
-                    // instead results in an infinite loop in the zip() case
-                    bool operator!=(const Iterator&) const {
-                        return false;
-                    }
-
-                    std::tuple<> operator*() {
-                        return std::tuple<>{};
-                    }
-            };
 
             Iterator begin() {
-                return {};
+                return this->make_begin(this->indices);
             }
 
             Iterator end() {
-                return {};
+                return this->make_end(this->indices);
             }
-    };
+        };
 
     template <typename... Containers>
     Zipped<Containers...> zip(Containers&&... containers) {
