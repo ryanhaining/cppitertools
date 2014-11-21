3bab276571b568464d91b3ff9d5ea4eb86889a5a
Ryan Haining <rhainin1@binghamton.edu>
2014-11-20 21:07:57 -0500

    adds more serious tests for enumerate

--- catchtest/test_enumerate.cpp
+++ catchtest/test_enumerate.cpp
@@ -4,6 +4,16 @@
 #include <string>
 #include <iterator>
 #include <utility>
+#include <sstream>
+
+namespace Catch {
+template <typename A, typename B>
+std::string toString( const std::pair<A, B>& p) {
+    std::ostringstream oss;
+    oss << '{' << p.first << ", " << p.second << '}';
+    return oss.str();
+}
+}
 
 #include "catch.hpp"
 
@@ -23,5 +33,43 @@ TEST_CASE("Empty", "[enumerate]") {
     std::string emp{};
     auto e = enumerate(emp);
     Vec v(std::begin(e), std::end(e));
+
     REQUIRE( v.empty() );
+}
+
+TEST_CASE("Modifications through enumerate affect container", "[enumerate]") {
+    std::vector<int> v{1, 2, 3, 4};
+    std::vector<int> vc(v.size(), -1);
+    for (auto&& p : enumerate(v)){
+        p.second = -1;
+    }
+
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("Static array works", "[enumerate]") {
+    char arr[] = {'w', 'x', 'y'};
+
+    SECTION("Conversion to vector") {
+        auto e = enumerate(arr);
+        Vec v(std::begin(e), std::end(e));
+        Vec vc{{0, 'w'}, {1, 'x'}, {2, 'y'}};
+        REQUIRE( v == vc );
+    }
+
+    SECTION("Modification through enumerate") {
+        for (auto&& p : enumerate(arr)) {
+            p.second = 'z';
+        }
+        std::vector<char> v(std::begin(arr), std::end(arr));
+        decltype(v) vc(v.size(), 'z');
+        REQUIRE( v == vc );
+    }
+}
+
+TEST_CASE("initializer_list works", "[enumerate]") {
+    auto e = enumerate({'a', 'b', 'c'});
+    Vec v(std::begin(e), std::end(e));
+    Vec vc{{0, 'a'}, {1, 'b'}, {2, 'c'}};
+    REQUIRE( v == vc );
 }
