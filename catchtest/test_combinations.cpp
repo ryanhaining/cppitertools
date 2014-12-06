38121285798f169c2a3b264712bac8740ebbc248
Ryan Haining <rhainin1@binghamton.edu>
2014-12-06 01:11:50 -0500

    Adds combinations test where length is too big

--- catchtest/test_combinations.cpp
+++ catchtest/test_combinations.cpp
@@ -12,8 +12,8 @@
 using iter::combinations;
 using CharCombSet = std::multiset<std::vector<char>>;
 
-TEST_CASE("Simple combination of 4", "[combinations]") {
-    std::string s = "ABCD";
+TEST_CASE("combinations: Simple combination of 4", "[combinations]") {
+    std::string s{"ABCD"};
     CharCombSet sc;
     for (auto v : combinations(s, 2)) {
         std::vector<char> vcopy(std::begin(v), std::end(v));
@@ -24,4 +24,9 @@ TEST_CASE("Simple combination of 4", "[combinations]") {
         {{'A','B'}, {'A','C'}, {'A','D'}, {'B','C'}, {'B','D'}, {'C','D'}};
     REQUIRE( ans == sc );
 }
-        
+
+TEST_CASE("combinations: size too large gives no results", "[combinations]") {
+    std::string s{"ABCD"};
+    auto c = combinations(s, 5);
+    REQUIRE( !(std::begin(c) != std::end(c)) );
+}
