8982da1f234161064e0296d61a37d503226adacc
Ryan Haining <rhainin1@binghamton.edu>
2014-12-12 17:37:29 -0500

    tests that dropwhile binds and moves correctly

--- catchtest/test_dropwhile.cpp
+++ catchtest/test_dropwhile.cpp
@@ -48,3 +48,29 @@ TEST_CASE("dropwhile: only drops from beginning", "[dropwhile]") {
     Vec vc = {5,6,5,4,3,2,1};
     REQUIRE( v == vc );
 }
+
+namespace {
+    int less_than_five(int i) {
+        return i < 5;
+    }
+}
+
+TEST_CASE("dropwhile: works with function pointer", "[dropwhile]") {
+    Vec ns{1,2,3,4,5,6,7,8};
+    auto d = dropwhile(less_than_five, ns);
+    Vec v(std::begin(d), std::end(d));
+    Vec vc = {5,6,7,8};
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("dropwhile: binds to lvalues, moves rvalues", "[dropwhile]") {
+    itertest::BasicIterable<int> bi{1,2,3,4};
+    SECTION("binds to lvalues") {
+        dropwhile(less_than_five, bi);
+        REQUIRE_FALSE( bi.was_moved_from() );
+    }
+    SECTION("moves rvalues") {
+        dropwhile(less_than_five, std::move(bi));
+        REQUIRE( bi.was_moved_from() );
+    }
+}
