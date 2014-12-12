552901063b31d85993ea415bdd3014d795550a76
Ryan Haining <rhainin1@binghamton.edu>
2014-12-11 20:50:57 -0500

    adds true and false run tests for compress

--- catchtest/test_compress.cpp
+++ catchtest/test_compress.cpp
@@ -24,3 +24,23 @@ TEST_CASE("compress: alternating", "[compress]") {
 
     REQUIRE( v  == vc );
 }
+
+TEST_CASE("compress: consecutive falses", "[compress]") {
+    std::vector<int> ivec{1, 2, 3, 4, 5};
+    std::vector<bool> bvec{true, false, false, false, true};
+    auto c = compress(ivec, bvec);
+    Vec v(std::begin(c), std::end(c));
+    Vec vc = {1,5};
+
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("compress: consecutive trues", "[compress]") {
+    std::vector<int> ivec{1, 2, 3, 4, 5};
+    std::vector<bool> bvec{false, true, true, true, false};
+    auto c = compress(ivec, bvec);
+    Vec v(std::begin(c), std::end(c));
+    Vec vc = {2,3,4}
+
+    REQUIRE( v == vc );
+}
