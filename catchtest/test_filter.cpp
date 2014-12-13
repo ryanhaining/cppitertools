e219b44fd3f5807a137e7278acb2a5ba495498f7
Ryan Haining <rhainin1@binghamton.edu>
2014-12-13 14:11:57 -0500

    adds filter catch tests

---
+++ catchtest/test_filter.cpp
@@ -0,0 +1,54 @@
+#include <filter.hpp>
+
+#include "helpers.hpp"
+
+#include <vector>
+#include <string>
+#include <iterator>
+
+#include "catch.hpp"
+
+using iter::filter;
+
+using Vec = const std::vector<int>;
+
+namespace {
+    bool less_than_five(int i) {
+        return i < 5;
+    }
+
+    class LessThanValue {
+        private:
+            int compare_val;
+
+        public:
+            LessThanValue(int v) : compare_val(v) { }
+
+            bool operator() (int i) {
+                return i < this->compare_val;
+            }
+    };
+}
+
+TEST_CASE("filter: handles different functor types", "[filter]") {
+    Vec ns = {1,2, 5,6, 3,1, 7, -1, 5};
+    Vec vc = {1,2,3,1,-1};
+    SECTION("with function pointer") {
+        auto f = filter(less_than_five, ns);
+        Vec v(std::begin(f), std::end(f));
+        REQUIRE( v == vc );
+    }
+
+    SECTION("with callable object") {
+        auto f = filter(LessThanValue{5}, ns);
+        Vec v(std::begin(f), std::end(f));
+        REQUIRE( v == vc );
+    }
+
+    SECTION("with lambda") {
+        auto ltf = [](int i) {return i < 5;};
+        auto f = filter(ltf, ns);
+        Vec v(std::begin(f), std::end(f));
+        REQUIRE( v == vc );
+    }
+}
