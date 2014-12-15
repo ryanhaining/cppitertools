d6a119c018321685d554576616a8e094c92daccf
Ryan Haining <rhainin1@binghamton.edu>
2014-12-14 22:29:59 -0500

    tests that groups may be skipped

--- catchtest/test_groupby.cpp
+++ catchtest/test_groupby.cpp
@@ -14,15 +14,16 @@ namespace {
     int length(const std::string& s) {
         return s.size();
     }
-}
 
-TEST_CASE("groupby: groups words by length") {
     const std::vector<std::string> vec = {
         "hi", "ab", "ho",
         "abc", "def",
         "abcde", "efghi"
     };
 
+}
+
+TEST_CASE("groupby: groups words by length") {
     std::vector<int> keys;
     std::vector<std::vector<std::string>> groups;
     for (auto gb : groupby(vec, &length)) {
@@ -36,6 +37,53 @@ TEST_CASE("groupby: groups words by length") {
     const std::vector<std::vector<std::string>> gc = {
         {"hi", "ab", "ho"},
         {"abc", "def"},
+        {"abcde", "efghi"},
+    };
+
+    REQUIRE( groups == gc );
+}
+
+TEST_CASE("groupby: groups can be skipped completely", "[groupby]") {
+    std::vector<int> keys;
+    std::vector<std::vector<std::string>> groups;
+    for (auto gb : groupby(vec, &length)) {
+        if (gb.first == 3) {
+            continue;
+        }
+        keys.push_back(gb.first);
+        groups.emplace_back(std::begin(gb.second), std::end(gb.second));
+    }
+
+    const std::vector<int> kc = {2, 5};
+    REQUIRE( keys == kc );
+
+    const std::vector<std::vector<std::string>> gc = {
+        {"hi", "ab", "ho"},
+        {"abcde", "efghi"},
+    };
+
+    REQUIRE( groups == gc );
+}
+
+TEST_CASE("groupby: groups can be skipped partially", "[groupby]") {
+    std::vector<int> keys;
+    std::vector<std::vector<std::string>> groups;
+    for (auto gb : groupby(vec, &length)) {
+        keys.push_back(gb.first);
+        if (gb.first == 3) {
+            std::vector<std::string> cut_short = {*std::begin(gb.second)};
+            groups.push_back(cut_short);
+        } else {
+            groups.emplace_back(std::begin(gb.second), std::end(gb.second));
+        }
+    }
+
+    const std::vector<int> kc = {2, 3, 5};
+    REQUIRE( keys == kc );
+
+    const std::vector<std::vector<std::string>> gc = {
+        {"hi", "ab", "ho"},
+        {"abc"},
         {"abcde", "efghi"},
     };
 
