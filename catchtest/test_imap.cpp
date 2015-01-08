a1ea801b42a283f816d5eeb4ce5634978ef1ba19
Ryan Haining <rhainin1@binghamton.edu>
2015-01-08 17:28:55 -0500

    tests imap with multiple sequences

--- catchtest/test_imap.cpp
+++ catchtest/test_imap.cpp
@@ -22,6 +22,14 @@ namespace {
                 return i + 1;
             }
     };
+
+    int power(int b, int e) {
+        int acc = 1;
+        for (int i = 0; i < e; ++i) {
+            acc *= b;
+        }
+        return acc;
+    }
 }
 
 TEST_CASE("imap: works with lambda, callable, and function", "[imap]") {
@@ -30,21 +38,32 @@ TEST_CASE("imap: works with lambda, callable, and function", "[imap]") {
         auto im = imap([](int i) { return i + 1; }, ns);
         Vec v(std::begin(im), std::end(im));
         Vec vc = {11, 21, 31};
-        REQUIRE(v == vc);
+        REQUIRE( v == vc );
     }
 
     SECTION("with function") {
         auto im = imap(plusone, ns);
         Vec v(std::begin(im), std::end(im));
         Vec vc = {11, 21, 31};
-        REQUIRE(v == vc);
+        REQUIRE( v == vc );
     }
 
     SECTION("with function") {
         auto im = imap(PlusOner{}, ns);
         Vec v(std::begin(im), std::end(im));
         Vec vc = {11, 21, 31};
-        REQUIRE(v == vc);
+        REQUIRE( v == vc );
     }
 
+}
+
+TEST_CASE("imap: works with multiple sequences", "[imap]") {
+    Vec bases = {0, 1, 2, 3};
+    Vec exps = {1, 2, 3, 4};
+
+    auto im = imap(power, bases, exps);
+    Vec v(std::begin(im), std::end(im));
+    Vec vc = {0, 1, 8, 81};
+
+    REQUIRE( v == vc );
 }
