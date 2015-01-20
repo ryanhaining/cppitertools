f01bb1b69062d79923c636386e7b405bed79fb85
Ryan Haining <rhainin1@binghamton.edu>
2015-01-20 14:33:13 -0500

    tests reversed with array

--- catchtest/test_reversed.cpp
+++ catchtest/test_reversed.cpp
@@ -9,8 +9,18 @@
 using iter::reversed;
 using Vec = const std::vector<int>;
 
-TEST_CASE("Reversing a vector", "[reversed]") {
+TEST_CASE("reversed: can reverse a vector", "[reversed]") {
     Vec ns = {10, 20, 30, 40};
+    auto r = reversed(ns);
+
+    Vec v(std::begin(r), std::end(r));
+    Vec vc = {40, 30, 20, 10};
+
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("reversed: can reverse an array", "[reversed]") {
+    int ns[] = {10, 20, 30, 40};
     auto r = reversed(ns);
 
     Vec v(std::begin(r), std::end(r));
