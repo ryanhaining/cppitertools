1c2a4bbea86ed71ae4e5cf241f9c2c833d39c20f
Ryan Haining <rhainin1@binghamton.edu>
2015-01-26 15:11:22 -0500

    tests unique_everseen with adjacent duplicates

---
+++ catchtest/test_unique_everseen.cpp
@@ -0,0 +1,21 @@
+#include <unique_everseen.hpp>
+
+#include "helpers.hpp"
+
+#include <vector>
+#include <string>
+#include <iterator>
+
+#include "catch.hpp"
+
+using iter::unique_everseen;
+
+using Vec = const std::vector<int>;
+
+TEST_CASE("unique everseen: adjacent repeating values", "[unique_everseen]") {
+        Vec ns = {1,1,1,2,2,3,4,4,5,6,7,8,8,8,8,9,9};
+        auto ue = unique_everseen(ns);
+        Vec v(std::begin(ue), std::end(ue));
+        Vec vc = {1,2,3,4,5,6,7,8,9};
+        REQUIRE( v == vc );
+}
