4eb2423252772e26f60491c322a7051b06bebdfb
Ryan Haining <rhainin1@binghamton.edu>
2015-01-26 15:15:33 -0500

    tests unique_everseen with non-adjacent duplicates

--- catchtest/test_unique_everseen.cpp
+++ catchtest/test_unique_everseen.cpp
@@ -13,9 +13,18 @@ using iter::unique_everseen;
 using Vec = const std::vector<int>;
 
 TEST_CASE("unique everseen: adjacent repeating values", "[unique_everseen]") {
-        Vec ns = {1,1,1,2,2,3,4,4,5,6,7,8,8,8,8,9,9};
-        auto ue = unique_everseen(ns);
-        Vec v(std::begin(ue), std::end(ue));
-        Vec vc = {1,2,3,4,5,6,7,8,9};
-        REQUIRE( v == vc );
+    Vec ns = {1,1,1,2,2,3,4,4,5,6,7,8,8,8,8,9,9};
+    auto ue = unique_everseen(ns);
+    Vec v(std::begin(ue), std::end(ue));
+    Vec vc = {1,2,3,4,5,6,7,8,9};
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("unique everseen: nonadjacent repeating values",
+        "[unique_everseen]") {
+    Vec ns = {1,2,3,4,3,2,1,5,6};
+    auto ue = unique_everseen(ns);
+    Vec v(std::begin(ue), std::end(ue));
+    Vec vc = {1,2,3,4,5,6};
+    REQUIRE( v == vc );
 }
