e233994718d75ab232cb795f6d4394b1b6e829c2
Ryan Haining <rhainin1@binghamton.edu>
2015-01-26 15:44:34 -0500

    tests unique_justseen with non-adjacent dupes

--- catchtest/test_unique_justseen.cpp
+++ catchtest/test_unique_justseen.cpp
@@ -19,3 +19,19 @@ TEST_CASE("unique justseen: adjacent repeating values", "[unique_justseen]") {
     Vec vc = {1,2,3,4,5,6,7,8,9};
     REQUIRE( v == vc );
 }
+
+TEST_CASE("unique justseen: some repeating values", "[unique_justseen]") {
+    Vec ns = {1,2,2,3,4,4,5,6,6};
+    auto ue = unique_justseen(ns);
+    Vec v(std::begin(ue), std::end(ue));
+    Vec vc = {1,2,3,4,5,6};
+    REQUIRE( v == vc );
+}
+
+TEST_CASE("unique justseen: doesn't omit non-adjacent duplicates",
+        "[unique_justseen]") {
+    Vec ns = {1,2,1,2,1};
+    auto ue = unique_justseen(ns);
+    Vec v(std::begin(ue), std::end(ue));
+    REQUIRE( v == ns );
+}
