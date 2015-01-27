626b08b91be9692c7d14965e5e246cbc0d7a4513
Ryan Haining <rhainin1@binghamton.edu>
2015-01-27 14:37:11 -0500

    tests slice with just stop

---
+++ catchtest/test_slice.cpp
@@ -0,0 +1,21 @@
+#include <slice.hpp>
+
+#include <vector>
+#include <string>
+#include <utility>
+
+#include "helpers.hpp"
+#include "catch.hpp"
+
+using iter::slice;
+using Vec = const std::vector<int>;
+
+TEST_CASE("slice: take from beginning", "[slice]") {
+    Vec ns = {10,11,12,13,14,15,16,17,18,19};
+    auto sl = slice(ns, 5);
+
+    Vec v(std::begin(sl), std::end(sl));
+    Vec vc = {10,11,12,13,14};
+
+    REQUIRE( v == vc );
+}
