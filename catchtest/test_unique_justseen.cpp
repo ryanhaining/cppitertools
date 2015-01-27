6392b1cf180c30e1a94fe6c3f697dc5bb51d6955
Ryan Haining <rhainin1@binghamton.edu>
2015-01-26 23:11:47 -0500

    tests unique_justseen binds and moves correctly

--- catchtest/test_unique_justseen.cpp
+++ catchtest/test_unique_justseen.cpp
@@ -30,8 +30,17 @@ TEST_CASE("unique justseen: some repeating values", "[unique_justseen]") {
 
 TEST_CASE("unique justseen: doesn't omit non-adjacent duplicates",
         "[unique_justseen]") {
-    Vec ns = {1,2,1,2,1};
+    Vec ns = {1,2,3,2,1,2,3,2,1};
     auto ue = unique_justseen(ns);
     Vec v(std::begin(ue), std::end(ue));
     REQUIRE( v == ns );
+}
+
+TEST_CASE("unique justseen: moves and binds correctly", "[unique_justseen]") {
+    itertest::BasicIterable<int> bi{1, 2};
+    unique_justseen(bi);
+    REQUIRE_FALSE( bi.was_moved_from() );
+
+    unique_justseen(std::move(bi));
+    REQUIRE( bi.was_moved_from() );
 }
