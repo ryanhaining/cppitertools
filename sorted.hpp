9b120bb1b4d8010ec4dbf4ea5baa9e35ad1d487f
Ryan Haining <rhainin1@binghamton.edu>
2015-02-09 15:39:29 -0500

    uses return type deduction

--- sorted.hpp
+++ sorted.hpp
@@ -66,13 +66,10 @@ namespace iter {
     }
 
     template <typename Container>
-    auto sorted(Container&& container) ->
-             decltype(sorted(std::forward<Container>(container),
-                         std::less<iterator_deref<Container>>()))
-     {
-         return sorted(std::forward<Container>(container),
-                 std::less<iterator_deref<Container>>());
-     }
+    auto sorted(Container&& container) {
+        return sorted(std::forward<Container>(container),
+                std::less<iterator_deref<Container>>());
+    }
 
 }
 
