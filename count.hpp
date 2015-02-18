14c5246a13e6a4fdcec561d08e3aca1ba9430de6
Ryan Haining <haining.cpp@gmail.com>
2015-02-17 20:02:42 -0500

    uses default argument to eliminate count overload

--- count.hpp
+++ count.hpp
@@ -7,13 +7,6 @@
 
 namespace iter {
 
-    using DefaultRangeType = long;
-
-    auto count() -> decltype(range(DefaultRangeType(0), DefaultRangeType(0))) {
-        return range(DefaultRangeType(0),
-                std::numeric_limits<DefaultRangeType>::max());
-    }
-
     template <typename T>
     auto count(T start, T step) -> decltype(range(start, start, start)) {
         // if step is < 0, set the stop to numeric min, otherwise numeric max
@@ -22,8 +15,8 @@ namespace iter {
         return range(start, stop, step);
     }
 
-    template <typename T>
-    auto count(T start) -> decltype(range(start, start)) {
+    template <typename T =long>
+    auto count(T start =T(0)) -> decltype(range(start, start)) {
         return count(start, T(1));
     }
 }
