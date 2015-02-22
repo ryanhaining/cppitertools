c319227ec542468cd746d14ee986e113f6cad62b
Ryan Haining <haining.cpp@gmail.com>
2015-02-21 20:11:24 -0500

    tests IsIterator tests

--- catchtest/test_helpers.cpp
+++ catchtest/test_helpers.cpp
@@ -1,17 +1,82 @@
-#include "helpers.hpp"
 #include <utility>
 
 #include "catch.hpp"
+#include "helpers.hpp"
 
 using itertest::SolidInt;
+using itertest::IsIterator;
 
-TEST_CASE("SolidInt can be moved only once", "[helpers]") {
-    SolidInt i{3};
-    SECTION("Doesn't throw on first move") {
-        REQUIRE_NOTHROW( SolidInt{std::move(i)} );
-    }
-    SECTION("Throws on second move") {
-        SolidInt i2{std::move(i)};
-        REQUIRE_THROWS( SolidInt i3{std::move(i2)} );
-    }
+namespace {
+
+class ValidIter {
+    private:
+        int i;
+    public:
+        ValidIter& operator++(); // prefix
+        ValidIter operator++(int); // postfix
+        bool operator==(const ValidIter&) const;
+        bool operator!=(const ValidIter&) const;
+        int operator*();
+};
+
+}
+
+TEST_CASE("IsIterator fails when missing prefix ++", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        InvalidIter& operator++() = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing postfix ++", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        InvalidIter operator++(int) = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing ==", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        bool operator==(const InvalidIter&) const = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing !=", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        bool operator!=(const InvalidIter&) const = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing *", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        int operator*() = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing copy-ctor", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        InvalidIter(const InvalidIter&) = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator fails when missing copy assignment", "[helpers]") {
+    struct InvalidIter : ValidIter {
+        InvalidIter& operator=(const InvalidIter&) = delete;
+    };
+
+    REQUIRE( !IsIterator<InvalidIter>::value );
+}
+
+TEST_CASE("IsIterator passes a valid iterator", "[helpers]") {
+    REQUIRE( IsIterator<ValidIter>::value );
 }
