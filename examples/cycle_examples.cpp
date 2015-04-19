c295d8abf622aa8633b91c13b78165ca754f20a4
Ryan Haining <haining.cpp@gmail.com>
2015-04-18 18:05:50 -0700

    cycle_examples added

---
+++ examples/cycle_examples.cpp
@@ -0,0 +1,29 @@
+#include <cycle.hpp>
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main() {
+    std::cout << "cycle({2, 4, 6}) run 20 times:\n";
+    std::vector<int> vec = {2, 4, 6};
+    size_t count = 0;
+    for (auto&& i : iter::cycle(vec)) {
+        std::cout << i << '\n';
+        if (count == 20) {
+            break;
+        }
+        ++count;
+    }
+
+    std::cout << "cycle(\"hello\") run 20 times:\n";
+    count = 0;
+    const std::string s("hello");
+    for (auto&& c : iter::cycle(s)) {
+        std::cout << c << '\n';
+        if (count == 20) {
+            break;
+        }
+        ++count;
+    }
+}
