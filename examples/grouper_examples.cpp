b9352fe91e828c30c157e28efc85f8f0205aea5d
Ryan Haining <haining.cpp@gmail.com>
2015-05-01 18:57:55 -0700

    replaces testgrouper with grouper examples

---
+++ examples/grouper_examples.cpp
@@ -0,0 +1,23 @@
+#include <grouper.hpp>
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "chunk size: 4\n";
+    std::vector<int> v {1,2,3,4,5,6,7,8,9}; 
+    for (auto&& sec : iter::grouper(v, 4)) {
+        for (auto&& i : sec) {
+            std::cout << i << " ";
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << "chunk size: 3\n";
+    for (auto&& sec : iter::grouper(v,3)) {
+        for (auto&& i : sec) {
+            std::cout << i << " ";
+        }
+        std::cout << '\n';
+    }
+}
