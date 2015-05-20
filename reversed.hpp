128806ee5a5d26ca94a5f0538afbaaded59bee65
Ryan Haining <haining@google.com>
2015-05-20 10:56:06 -0700

    uses std::rbegin and rend, removes specialization
    Instead of using .rbegin() and .rend(), uses non-member std::rbegin() and
    std::rend() to get the iterators.  This also means (like replacing
    .begin() with std::begin() forever ago) that Reverser can handle arrays.
    This commit removes the array specialization.

--- reversed.hpp
+++ reversed.hpp
@@ -13,13 +13,12 @@ namespace iter {
     template <typename Container>
     Reverser<Container> reversed(Container&&);
 
-
     template <typename Container>
     class Reverser {
         private:
             Container container;
             friend Reverser reversed<Container>(Container&&);
-            
+
             Reverser(Container&& in_container)
                 : container(std::forward<Container>(in_container))
             { }
@@ -27,20 +26,20 @@ namespace iter {
         public:
             class Iterator : public std::iterator<
                          std::input_iterator_tag,
-                         iterator_traits_deref<Container>>
+                         reverse_iterator_traits_deref<Container>>
             {
                 private:
                     reverse_iterator_type<Container> sub_iter;
                 public:
-                    Iterator (reverse_iterator_type<Container>&& iter)
+                    Iterator(reverse_iterator_type<Container>&& iter)
                         : sub_iter{std::move(iter)}
-                    { } 
+                    { }
 
                     reverse_iterator_deref<Container> operator*() {
                         return *this->sub_iter;
                     }
 
-                    Iterator& operator++() { 
+                    Iterator& operator++() {
                         ++this->sub_iter;
                         return *this;
                     }
@@ -61,11 +60,11 @@ namespace iter {
             };
 
             Iterator begin() {
-                return {this->container.rbegin()};
+                return {std::rbegin(this->container)};
             }
 
             Iterator end() {
-                return {this->container.rend()};
+                return {std::rend(this->container)};
             }
 
     };
@@ -74,74 +73,6 @@ namespace iter {
     Reverser<Container> reversed(Container&& container) {
         return {std::forward<Container>(container)};
     }
-
-    //
-    // specialization for statically allocated arrays
-    //
-    template <typename T, std::size_t N>
-    Reverser<T[N]> reversed(T (&)[N]);
-
-    template <typename T, std::size_t N>
-    class Reverser<T[N]> {
-        private:
-            T *array;
-            friend Reverser reversed<T, N>(T (&)[N]);
-            
-            // Value constructor for use only in the reversed function
-            Reverser(T *in_array)
-                : array{in_array}
-            { }
-
-        public:
-            Reverser(const Reverser&) = default;
-            class Iterator : public std::iterator<std::input_iterator_tag, T>
-            {
-                private:
-                    T *sub_iter;
-                public:
-                    Iterator (T *iter)
-                        : sub_iter{iter}
-                    { } 
-
-                    iterator_deref<T[N]> operator*() {
-                        return *(this->sub_iter - 1);
-                    }
-
-                    Iterator& operator++() { 
-                        --this->sub_iter;
-                        return *this;
-                    }
-
-                    Iterator operator++(int) {
-                        auto ret = *this;
-                        ++*this;
-                        return ret;
-                    }
-
-                    bool operator!=(const Iterator& other) const {
-                        return this->sub_iter != other.sub_iter;
-                    }
-
-                    bool operator==(const Iterator& other) const {
-                        return !(*this != other);
-                    }
-            };
-
-            Iterator begin() {
-                return {this->array + N};
-            }
-
-            Iterator end() {
-            return {this->array};
-            }
-
-    };
-
-    template <typename T, std::size_t N>
-    Reverser<T[N]> reversed(T (&array)[N]) {
-        return {array};
-    }
-
 }
 
 #endif
