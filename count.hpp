144ff04e052604c2dc813a75d0b4a152ae598070
Ryan Haining <haining.cpp@gmail.com>
2015-06-29 22:16:40 -0700

    adds constexpr to count()

--- count.hpp
+++ count.hpp
@@ -8,15 +8,19 @@
 namespace iter {
 
     template <typename T>
-    auto count(T start, T step) -> decltype(range(start, start, start)) {
-        // if step is < 0, set the stop to numeric min, otherwise numeric max
-        T stop = step < T(0) ? std::numeric_limits<T>::min() :
-                            std::numeric_limits<T>::max();
-        return range(start, stop, step);
+    constexpr auto count(T start, T step)
+            -> decltype(range(start, start, start)) {
+        // if step is < 0, stop is numeric min, otherwise numeric max
+        return range(
+                start,
+                step < T(0) ? std::numeric_limits<T>::min() :
+                    std::numeric_limits<T>::max(),
+                step);
     }
 
     template <typename T =long>
-    auto count(T start =T(0)) -> decltype(range(start, start)) {
+    constexpr auto count(T start =T(0))
+            -> decltype(range(start, start)) {
         return count(start, T(1));
     }
 }
