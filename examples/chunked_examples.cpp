0d6be7da306e9da6b84c0f5532ffcb24380e227a
Ryan Haining <haining.cpp@gmail.com>
2015-08-20 23:18:11 -0700

    renames grouper_examples to chunked_examples

--- examples/chunked_examples.cpp
+++ examples/chunked_examples.cpp
@@ -1,4 +1,4 @@
-#include <grouper.hpp>
+#include <chunked.hpp>
 
 #include <iostream>
 #include <vector>
@@ -6,7 +6,7 @@
 int main() {
     std::cout << "chunk size: 4\n";
     std::vector<int> v {1,2,3,4,5,6,7,8,9}; 
-    for (auto&& sec : iter::grouper(v, 4)) {
+    for (auto&& sec : iter::chunked(v, 4)) {
         for (auto&& i : sec) {
             std::cout << i << " ";
         }
@@ -14,7 +14,7 @@ int main() {
     }
 
     std::cout << "chunk size: 3\n";
-    for (auto&& sec : iter::grouper(v,3)) {
+    for (auto&& sec : iter::chunked(v,3)) {
         for (auto&& i : sec) {
             std::cout << i << " ";
         }
