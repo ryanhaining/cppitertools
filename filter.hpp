99cae928d3b501a1a1dd7ad8ab1432af15760854
Ryan Haining <haining.cpp@gmail.com>
2015-08-20 21:26:56 -0700

    moves Filtered into impl { }

--- filter.hpp
+++ filter.hpp
@@ -8,125 +8,126 @@
 #include <initializer_list>
 
 namespace iter {
+  namespace impl {
+    template <typename FilterFunc, typename Container>
+    class Filtered;
+  }
 
-  // Forward declarations of Filter and filter
   template <typename FilterFunc, typename Container>
-  class Filter;
-
-  template <typename FilterFunc, typename Container>
-  Filter<FilterFunc, Container> filter(FilterFunc, Container&&);
+  impl::Filtered<FilterFunc, Container> filter(FilterFunc, Container&&);
 
   template <typename FilterFunc, typename T>
-  Filter<FilterFunc, std::initializer_list<T>> filter(
+  impl::Filtered<FilterFunc, std::initializer_list<T>> filter(
       FilterFunc, std::initializer_list<T>);
+}
 
-  template <typename FilterFunc, typename Container>
-  class Filter {
-   private:
-    Container container;
-    FilterFunc filter_func;
+template <typename FilterFunc, typename Container>
+class iter::impl::Filtered {
+ private:
+  Container container;
+  FilterFunc filter_func;
 
-    // The filter function is the only thing allowed to create a Filter
-    friend Filter filter<FilterFunc, Container>(FilterFunc, Container&&);
+  // The filter function is the only thing allowed to create a Filtered
+  friend Filtered iter::filter<FilterFunc, Container>(FilterFunc, Container&&);
 
-    template <typename FF, typename T>
-    friend Filter<FF, std::initializer_list<T>> filter(
-        FF, std::initializer_list<T>);
+  template <typename FF, typename T>
+  friend Filtered<FF, std::initializer_list<T>> iter::filter(
+      FF, std::initializer_list<T>);
 
-    // Value constructor for use only in the filter function
-    Filter(FilterFunc in_filter_func, Container&& in_container)
-        : container(std::forward<Container>(in_container)),
-          filter_func(in_filter_func) {}
+  // Value constructor for use only in the filter function
+  Filtered(FilterFunc in_filter_func, Container&& in_container)
+      : container(std::forward<Container>(in_container)),
+        filter_func(in_filter_func) {}
 
-   public:
-    class Iterator : public std::iterator<std::input_iterator_tag,
-                         iterator_traits_deref<Container>> {
-     protected:
-      using Holder = DerefHolder<iterator_deref<Container>>;
-      iterator_type<Container> sub_iter;
-      iterator_type<Container> sub_end;
-      Holder item;
-      FilterFunc* filter_func;
+ public:
+  class Iterator : public std::iterator<std::input_iterator_tag,
+                       iterator_traits_deref<Container>> {
+   protected:
+    using Holder = DerefHolder<iterator_deref<Container>>;
+    iterator_type<Container> sub_iter;
+    iterator_type<Container> sub_end;
+    Holder item;
+    FilterFunc* filter_func;
 
-      void inc_sub_iter() {
-        ++this->sub_iter;
-        if (this->sub_iter != this->sub_end) {
-          this->item.reset(*this->sub_iter);
-        }
+    void inc_sub_iter() {
+      ++this->sub_iter;
+      if (this->sub_iter != this->sub_end) {
+        this->item.reset(*this->sub_iter);
       }
-
-      // increment until the iterator points to is true on the
-      // predicate.  Called by constructor and operator++
-      void skip_failures() {
-        while (this->sub_iter != this->sub_end
-               && !(*this->filter_func)(this->item.get())) {
-          this->inc_sub_iter();
-        }
-      }
-
-     public:
-      Iterator(iterator_type<Container> iter, iterator_type<Container> end,
-          FilterFunc& in_filter_func)
-          : sub_iter{iter}, sub_end{end}, filter_func(&in_filter_func) {
-        if (this->sub_iter != this->sub_end) {
-          this->item.reset(*this->sub_iter);
-        }
-        this->skip_failures();
-      }
-
-      typename Holder::reference operator*() {
-        return this->item.get();
-      }
-
-      typename Holder::pointer operator->() {
-        return this->item.get_ptr();
-      }
-
-      Iterator& operator++() {
-        this->inc_sub_iter();
-        this->skip_failures();
-        return *this;
-      }
-
-      Iterator operator++(int) {
-        auto ret = *this;
-        ++*this;
-        return ret;
-      }
-
-      bool operator!=(const Iterator& other) const {
-        return this->sub_iter != other.sub_iter;
-      }
-
-      bool operator==(const Iterator& other) const {
-        return !(*this != other);
-      }
-    };
-
-    Iterator begin() {
-      return {std::begin(this->container), std::end(this->container),
-          this->filter_func};
     }
 
-    Iterator end() {
-      return {std::end(this->container), std::end(this->container),
-          this->filter_func};
+    // increment until the iterator points to is true on the
+    // predicate.  Called by constructor and operator++
+    void skip_failures() {
+      while (this->sub_iter != this->sub_end
+             && !(*this->filter_func)(this->item.get())) {
+        this->inc_sub_iter();
+      }
+    }
+
+   public:
+    Iterator(iterator_type<Container> iter, iterator_type<Container> end,
+        FilterFunc& in_filter_func)
+        : sub_iter{iter}, sub_end{end}, filter_func(&in_filter_func) {
+      if (this->sub_iter != this->sub_end) {
+        this->item.reset(*this->sub_iter);
+      }
+      this->skip_failures();
+    }
+
+    typename Holder::reference operator*() {
+      return this->item.get();
+    }
+
+    typename Holder::pointer operator->() {
+      return this->item.get_ptr();
+    }
+
+    Iterator& operator++() {
+      this->inc_sub_iter();
+      this->skip_failures();
+      return *this;
+    }
+
+    Iterator operator++(int) {
+      auto ret = *this;
+      ++*this;
+      return ret;
+    }
+
+    bool operator!=(const Iterator& other) const {
+      return this->sub_iter != other.sub_iter;
+    }
+
+    bool operator==(const Iterator& other) const {
+      return !(*this != other);
     }
   };
 
-  // Helper function to instantiate a Filter
-  template <typename FilterFunc, typename Container>
-  Filter<FilterFunc, Container> filter(
-      FilterFunc filter_func, Container&& container) {
-    return {filter_func, std::forward<Container>(container)};
+  Iterator begin() {
+    return {std::begin(this->container), std::end(this->container),
+        this->filter_func};
   }
 
-  template <typename FilterFunc, typename T>
-  Filter<FilterFunc, std::initializer_list<T>> filter(
-      FilterFunc filter_func, std::initializer_list<T> il) {
-    return {filter_func, std::move(il)};
+  Iterator end() {
+    return {std::end(this->container), std::end(this->container),
+        this->filter_func};
   }
+};
 
+template <typename FilterFunc, typename Container>
+iter::impl::Filtered<FilterFunc, Container> iter::filter(
+    FilterFunc filter_func, Container&& container) {
+  return {filter_func, std::forward<Container>(container)};
+}
+
+template <typename FilterFunc, typename T>
+iter::impl::Filtered<FilterFunc, std::initializer_list<T>> iter::filter(
+    FilterFunc filter_func, std::initializer_list<T> il) {
+  return {filter_func, std::move(il)};
+}
+
+namespace iter {
   namespace detail {
 
     template <typename T>
