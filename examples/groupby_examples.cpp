67abb0315fe75c6e7e1519b24985a1462f1bf420
ryanhaining <haining.cpp@gmail.com>
2015-08-24 13:05:26 -0700

    adds missing && in groupby examples

--- examples/groupby_examples.cpp
+++ examples/groupby_examples.cpp
@@ -35,17 +35,15 @@ int main() {
         }
         std::cout << '\n';
     }
-   
+
 
     std::cout << "ints grouped by their value:\n";
     std::vector<int> ivec = {5, 5, 6, 6, 19, 19, 19, 19, 69, 0, 10, 10};
-    for (auto gb : iter::groupby(ivec)) {
+    for (auto&& gb : iter::groupby(ivec)) {
         std::cout << "key(" << gb.first << "): ";
-        for (auto s : gb.second) {
+        for (auto&& s : gb.second) {
             std::cout << s << "  ";
         }
         std::cout << '\n';
     }
 }
-
-    
