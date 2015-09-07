cc3bcd3d0f0e53a877e3c176e52c357f172480e5
Ryan Haining <haining.cpp@gmail.com>
2015-09-06 23:25:52 -0700

    tests justseen impl has move ctor only

--- test/test_unique_justseen.cpp
+++ test/test_unique_justseen.cpp
@@ -13,40 +13,48 @@ using iter::unique_justseen;
 using Vec = const std::vector<int>;
 
 TEST_CASE("unique justseen: adjacent repeating values", "[unique_justseen]") {
-    Vec ns = {1,1,1,2,2,3,4,4,5,6,7,8,8,8,8,9,9};
-    auto ue = unique_justseen(ns);
-    Vec v(std::begin(ue), std::end(ue));
-    Vec vc = {1,2,3,4,5,6,7,8,9};
-    REQUIRE( v == vc );
+  Vec ns = {1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 8, 8, 8, 8, 9, 9};
+  auto ue = unique_justseen(ns);
+  Vec v(std::begin(ue), std::end(ue));
+  Vec vc = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  REQUIRE(v == vc);
 }
 
 TEST_CASE("unique justseen: some repeating values", "[unique_justseen]") {
-    Vec ns = {1,2,2,3,4,4,5,6,6};
-    auto ue = unique_justseen(ns);
-    Vec v(std::begin(ue), std::end(ue));
-    Vec vc = {1,2,3,4,5,6};
-    REQUIRE( v == vc );
+  Vec ns = {1, 2, 2, 3, 4, 4, 5, 6, 6};
+  auto ue = unique_justseen(ns);
+  Vec v(std::begin(ue), std::end(ue));
+  Vec vc = {1, 2, 3, 4, 5, 6};
+  REQUIRE(v == vc);
 }
 
 TEST_CASE("unique justseen: doesn't omit non-adjacent duplicates",
-        "[unique_justseen]") {
-    Vec ns = {1,2,3,2,1,2,3,2,1};
-    auto ue = unique_justseen(ns);
-    Vec v(std::begin(ue), std::end(ue));
-    REQUIRE( v == ns );
+    "[unique_justseen]") {
+  Vec ns = {1, 2, 3, 2, 1, 2, 3, 2, 1};
+  auto ue = unique_justseen(ns);
+  Vec v(std::begin(ue), std::end(ue));
+  REQUIRE(v == ns);
 }
 
 TEST_CASE("unique justseen: moves and binds correctly", "[unique_justseen]") {
-    itertest::BasicIterable<int> bi{1, 2};
-    unique_justseen(bi);
-    REQUIRE_FALSE( bi.was_moved_from() );
+  itertest::BasicIterable<int> bi{1, 2};
+  unique_justseen(bi);
+  REQUIRE_FALSE(bi.was_moved_from());
 
-    unique_justseen(std::move(bi));
-    REQUIRE( bi.was_moved_from() );
+  unique_justseen(std::move(bi));
+  REQUIRE(bi.was_moved_from());
 }
 
 TEST_CASE("unique_justseen: iterator meets requirements", "[unique_justseen]") {
-    std::string s{};
-    auto c = unique_justseen(s);
-    REQUIRE( itertest::IsIterator<decltype(std::begin(c))>::value );
+  std::string s{};
+  auto c = unique_justseen(s);
+  REQUIRE(itertest::IsIterator<decltype(std::begin(c))>::value);
+}
+
+template <typename T>
+using ImpT = decltype(unique_justseen(std::declval<T>()));
+TEST_CASE(
+    "unique_justseen: has correct ctor and assign ops", "[unique_justseen]") {
+  REQUIRE(itertest::IsMoveConstructibleOnly<ImpT<std::string&>>::value);
+  REQUIRE(itertest::IsMoveConstructibleOnly<ImpT<std::string>>::value);
 }
