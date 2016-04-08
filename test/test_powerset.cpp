40f8129e740577f1330b20f9d5b45f657347fd3d
Ryan Haining <haining.cpp@gmail.com>
2016-04-07 19:38:30 -0700

    tests powerset with pipe

--- test/test_powerset.cpp
+++ test/test_powerset.cpp
@@ -14,14 +14,19 @@ using IntPermSet = std::multiset<std::multiset<int>>;
 TEST_CASE("powerset: basic test, [1, 2, 3]", "[powerset]") {
   const std::vector<int> ns = {1, 2, 3};
   IntPermSet v;
-  for (auto&& st : powerset(ns)) {
-    v.emplace(std::begin(st), std::end(st));
+  SECTION("Normal call") {
+    for (auto&& st : powerset(ns)) {
+      v.emplace(std::begin(st), std::end(st));
+    }
+  }
+  SECTION("Pipe") {
+    for (auto&& st : ns | powerset) {
+      v.emplace(std::begin(st), std::end(st));
+    }
   }
 
-  const IntPermSet vc = {std::multiset<int>{}, {1}, {2},
-      {
-          3,
-      },
+  const IntPermSet vc = {
+    std::multiset<int>{}, {1}, {2}, {3},
       {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}};
   REQUIRE(v == vc);
 }
