a4971f935f226913266c1c3c91abe2f6e1ea4312
Ryan Haining <haining.cpp@gmail.com>
2016-04-28 23:11:55 -0700

    tests slice with pipe

--- test/test_slice.cpp
+++ test/test_slice.cpp
@@ -12,31 +12,52 @@ using Vec = const std::vector<int>;
 
 TEST_CASE("slice: take from beginning", "[slice]") {
   Vec ns = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-  auto sl = slice(ns, 5);
 
-  Vec v(std::begin(sl), std::end(sl));
+  std::vector<int> v;
+  SECTION("Normal call") {
+    auto sl = slice(ns, 5);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+  SECTION("Pipe") {
+    auto sl = ns | slice(5);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+
   Vec vc = {10, 11, 12, 13, 14};
-
   REQUIRE(v == vc);
 }
 
 TEST_CASE("slice: start and stop", "[slice]") {
   Vec ns = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-  auto sl = slice(ns, 2, 6);
 
-  Vec v(std::begin(sl), std::end(sl));
+  std::vector<int> v;
+  SECTION("Normal call") {
+    auto sl = slice(ns, 2, 6);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+  SECTION("Pipe") {
+    auto sl = ns | slice(2, 6);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+
   Vec vc = {12, 13, 14, 15};
-
   REQUIRE(v == vc);
 }
 
 TEST_CASE("slice: start, stop, step", "[slice]") {
   Vec ns = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-  auto sl = slice(ns, 2, 8, 2);
+  std::vector<int> v;
 
-  Vec v(std::begin(sl), std::end(sl));
+  SECTION("Normal call") {
+    auto sl = slice(ns, 2, 8, 2);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+  SECTION("Pipe") {
+    auto sl = ns | slice(2, 8, 2);
+    v.assign(std::begin(sl), std::end(sl));
+  }
+
   Vec vc = {12, 14, 16};
-
   REQUIRE(v == vc);
 }
 
