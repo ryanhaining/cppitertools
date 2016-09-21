07659eaef84cef9a3cc0a6304adbeac0f5df6bfe
Ryan Haining <haining.cpp@gmail.com>
2016-09-20 21:00:40 -0700

    Moves lfsr contents into ShuffledView as statics

--- shuffled.hpp
+++ shuffled.hpp
@@ -20,47 +20,10 @@ namespace iter {
   namespace impl {
     template <typename Container, typename Distance = std::size_t>
     class ShuffledView;
-    // linear feedback shift register
-    namespace lfsr {
-      constexpr std::array<std::uint16_t, 64> PRIME_POLY = {
-          {1, 3, 3, 3, 5, 3, 3, 29, 17, 9, 5, 83, 27, 43, 3, 45, 9, 129, 39, 9,
-              5, 3, 33, 27, 9, 71, 39, 9, 5, 83, 9, 197, 8193, 231, 5, 119, 83,
-              99, 17, 57, 9, 153, 89, 101, 27, 449, 33, 183, 113, 29, 75, 9, 71,
-              125, 71, 149, 129, 99, 123, 3, 39, 105, 3,
-              27}};  // prime poly for full-cycle shift registers
-      constexpr std::uint16_t get_approx(std::uint64_t val);
-      constexpr std::uint64_t shift(std::uint64_t reg, std::uint8_t reg_size);
-    }
   }
 
   template <typename Container, typename Distance = std::size_t>
   impl::ShuffledView<Container, Distance> shuffled(Container&&, int seed = 1);
-}
-
-// power of 2 approximation (val < pow(2, get_approx(val)+1))
-constexpr inline std::uint16_t iter::impl::lfsr::get_approx(std::uint64_t val) {
-  if (val == 0) {
-    return 0;
-  }
-  constexpr auto LIMIT = std::uint64_t(1UL) << 63;
-  if (val > LIMIT) {
-    return 63;
-  }
-  std::uint16_t pow2_approx = 0;
-  while (pow2_approx < 64 && (val >= (1UL << (pow2_approx + 1)))) {
-    ++pow2_approx;
-  }
-  return pow2_approx;
-}
-
-constexpr inline std::uint64_t iter::impl::lfsr::shift(
-    std::uint64_t reg, std::uint8_t reg_size) {
-  if (reg & 1) {
-    reg = ((reg ^ PRIME_POLY[reg_size]) >> 1) | (1 << reg_size);
-  } else {
-    reg >>= 1;
-  }
-  return reg;
 }
 
 template <typename Container, typename Distance>
@@ -77,17 +40,49 @@ class iter::impl::ShuffledView {
   ShuffledView(Container&& in_container, int in_seed)
       : container(std::forward<Container>(in_container)),
         size(std::distance(std::begin(container), std::end(container))),
-        size_approx(lfsr::get_approx(size)),
+        size_approx(get_approx(size)),
         seed(in_seed) {
     if (size > 0) {
       std::uint64_t mask = -1;
       mask = (mask >> (64 - size_approx));
       this->seed = seed & mask;
-      this->seed = lfsr::shift(this->seed, size_approx);
+      this->seed = shift(this->seed, size_approx);
       while (this->seed >= size) {
-        this->seed = lfsr::shift(this->seed, size_approx);
+        this->seed = shift(this->seed, size_approx);
       }
     }
+  }
+
+  // linear feedback shift register
+  static constexpr std::array<std::uint16_t, 64> PRIME_POLY = {{1, 3, 3, 3, 5,
+      3, 3, 29, 17, 9, 5, 83, 27, 43, 3, 45, 9, 129, 39, 9, 5, 3, 33, 27, 9, 71,
+      39, 9, 5, 83, 9, 197, 8193, 231, 5, 119, 83, 99, 17, 57, 9, 153, 89, 101,
+      27, 449, 33, 183, 113, 29, 75, 9, 71, 125, 71, 149, 129, 99, 123, 3, 39,
+      105, 3, 27}};  // prime poly for full-cycle shift registers
+  // power of 2 approximation (val < pow(2, get_approx(val)+1))
+  static constexpr inline std::uint16_t get_approx(std::uint64_t val) {
+    if (val == 0) {
+      return 0;
+    }
+    constexpr auto LIMIT = std::uint64_t(1UL) << 63;
+    if (val > LIMIT) {
+      return 63;
+    }
+    std::uint16_t pow2_approx = 0;
+    while (pow2_approx < 64 && (val >= (1UL << (pow2_approx + 1)))) {
+      ++pow2_approx;
+    }
+    return pow2_approx;
+  }
+
+  static constexpr inline std::uint64_t shift(
+      std::uint64_t reg, std::uint8_t reg_size) {
+    if (reg & 1) {
+      reg = ((reg ^ PRIME_POLY[reg_size]) >> 1) | (1 << reg_size);
+    } else {
+      reg >>= 1;
+    }
+    return reg;
   }
 
  public:
@@ -109,7 +104,7 @@ class iter::impl::ShuffledView {
         return *this;
       }
       do {
-        state = lfsr::shift(state, owner->size_approx);
+        state = shift(state, owner->size_approx);
       } while (state > owner->size);
       if (state == owner->seed) {
         *this = owner->end();
@@ -164,6 +159,9 @@ class iter::impl::ShuffledView {
     return {this, (state >= size ? seed : state + 1)};
   }
 };
+template <typename Container, typename Distance>
+constexpr std::array<std::uint16_t, 64>
+    iter::impl::ShuffledView<Container, Distance>::PRIME_POLY;
 
 template <typename Container, typename Distance = std::size_t>
 iter::impl::ShuffledView<Container, Distance> iter::shuffled(
