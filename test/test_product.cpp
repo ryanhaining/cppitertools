92043563da3f3bbe62838526cffca2add1bbf03f
ryanhaining <haining.cpp@gmail.com>
2017-07-28 11:21:11 -0700

    Adds product test with repeat argument.

--- test/test_product.cpp
+++ test/test_product.cpp
@@ -58,6 +58,27 @@ TEST_CASE("product: three sequences", "[product]") {
   REQUIRE(v == vc);
 }
 
+TEST_CASE("product: with repeat", "[product]") {
+  using TP = std::tuple<char, char, char>;
+  using ResType = const std::vector<TP>;
+  const std::string s = "hop";
+  auto p = product<3>(s);
+  ResType v(std::begin(p), std::end(p));
+
+  ResType vc = {
+      TP{'h', 'h', 'h'}, TP{'h', 'h', 'o'}, TP{'h', 'h', 'p'},
+      TP{'h', 'o', 'h'}, TP{'h', 'o', 'o'}, TP{'h', 'o', 'p'},
+      TP{'h', 'p', 'h'}, TP{'h', 'p', 'o'}, TP{'h', 'p', 'p'},
+      TP{'o', 'h', 'h'}, TP{'o', 'h', 'o'}, TP{'o', 'h', 'p'},
+      TP{'o', 'o', 'h'}, TP{'o', 'o', 'o'}, TP{'o', 'o', 'p'},
+      TP{'o', 'p', 'h'}, TP{'o', 'p', 'o'}, TP{'o', 'p', 'p'},
+      TP{'p', 'h', 'h'}, TP{'p', 'h', 'o'}, TP{'p', 'h', 'p'},
+      TP{'p', 'o', 'h'}, TP{'p', 'o', 'o'}, TP{'p', 'o', 'p'},
+      TP{'p', 'p', 'h'}, TP{'p', 'p', 'o'}, TP{'p', 'p', 'p'},
+  };
+  REQUIRE(v == vc);
+}
+
 TEST_CASE("product: empty when any iterable is empty", "[product]") {
   Vec n1 = {0, 1};
   Vec n2 = {0, 1, 2};
