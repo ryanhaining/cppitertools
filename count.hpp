dd34859b1cbd5fe8c7013fd0ab6888430fcaaef0
Ryan Haining <haining.cpp@gmail.com>
2019-06-23 12:32:41 -0400

    Replaces count's min() call with lowest()
    Fixes #60

--- count.hpp
+++ count.hpp
@@ -8,8 +8,7 @@
 namespace iter {
   template <typename T>
   constexpr auto count(T start, T step) noexcept {
-    // if step is < 0, set the stop to numeric min, otherwise numeric max
-    T stop = step < T(0) ? std::numeric_limits<T>::min()
+    T stop = step < T(0) ? std::numeric_limits<T>::lowest()
                          : std::numeric_limits<T>::max();
     return range(start, stop, step);
   }
--- test/test_count.cpp
+++ test/test_count.cpp
@@ -65,6 +65,68 @@ TEST_CASE("count: with step > 1", "[count]") {
   REQUIRE(v == vc);
 }
 
+TEST_CASE("count: with unsigned", "[count]") {
+  constexpr unsigned int uint_max =
+      static_cast<unsigned int>(std::numeric_limits<int>::max());
+  std::vector<unsigned int> v{};
+  int steps = 0;
+  for (auto i : count(uint_max)) {
+    v.push_back(i);
+    if (steps == 2) {
+      break;
+    }
+    ++steps;
+  }
+  const std::vector<unsigned int> vc{uint_max, uint_max + 1, uint_max + 2};
+
+  REQUIRE(v == vc);
+}
+
+TEST_CASE("count: with negative step", "[count]") {
+  std::vector<int> v{};
+  int steps = 0;
+  for (auto i : count(0, -1)) {
+    v.push_back(i);
+    if (steps == 2) {
+      break;
+    }
+    ++steps;
+  }
+  const std::vector<int> vc{0, -1, -2};
+
+  REQUIRE(v == vc);
+}
+
+TEST_CASE("count: with double", "[count]") {
+  std::vector<double> v{};
+  int steps = 0;
+  for (auto i : count(1.0, 0.5)) {
+    v.push_back(i);
+    if (steps == 3) {
+      break;
+    }
+    ++steps;
+  }
+  const std::vector<double> vc{1.0, 1.5, 2.0, 2.5};
+
+  REQUIRE(v == vc);
+}
+
+TEST_CASE("count: with negative double step", "[count]") {
+  std::vector<double> v{};
+  int steps = 0;
+  for (auto i : count(1.0, -0.5)) {
+    v.push_back(i);
+    if (steps == 3) {
+      break;
+    }
+    ++steps;
+  }
+  const std::vector<double> vc{1.0, 0.5, 0.0, -0.5};
+
+  REQUIRE(v == vc);
+}
+
 TEST_CASE("count: can bo constexpr", "[count]") {
   constexpr auto c = count();
   constexpr auto c2 = count(5);
