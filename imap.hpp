2cc82d2be666aabd66279a8693fd2c6f196117c1
ryanhaining <haining.cpp@gmail.com>
2019-12-26 15:04:25 -0800

    Adds information to imap return type. Fixes #66

--- imap.hpp
+++ imap.hpp
@@ -1,17 +1,21 @@
 #ifndef ITER_IMAP_H_
 #define ITER_IMAP_H_
 
+#include <utility>
+
 #include "starmap.hpp"
 #include "zip.hpp"
-
-#include <utility>
 
 namespace iter {
   namespace impl {
     struct IMapFn : PipeableAndBindFirst<IMapFn> {
       template <typename MapFunc, typename... Containers>
-      decltype(auto) operator()(
-          MapFunc map_func, Containers&&... containers) const {
+      auto operator()(MapFunc map_func, Containers&&... containers) const
+          // explicitly specifying type here to allow more expressions that only
+          // care about the type, and don't need a valid implementation.
+          // See #66
+          -> StarMapper<MapFunc,
+              decltype(zip(std::forward<Containers>(containers)...))> {
         return starmap(map_func, zip(std::forward<Containers>(containers)...));
       }
       using PipeableAndBindFirst<IMapFn>::operator();
