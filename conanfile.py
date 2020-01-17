2547739b3ca1c1f08071b4c85df57f160df1f111
Alexander Bigerl <bigerl@mail.uni-paderborn.de>
2020-01-17 14:24:46 +0100

    updated conan meta data

--- conanfile.py
+++ conanfile.py
@@ -3,23 +3,27 @@ from conans import ConanFile, CMake
 import os
 
 
-class Tentris_Parser(ConanFile):
+class CppIterTools(ConanFile):
     name = "cppitertools"
-    version = "1.0"
-    author = "Ryan Haining"
-    description = "Implementation of python itertools and builtin iteration functions for C++17 "
-    topics = ("please add topics here")
+    version = "2.0"
+    author = "Ryan Haining <haining.cpp@gmail.com>"
+    homepage = "https://github.com/ryanhaining/cppitertools"
+    url = homepage
+    topics = ("conan", "itertools", "cppitertools")
+    license = 'BSD 2-Clause "Simplified" License'
+    description = "Range-based for loop add-ons inspired by the Python builtins and itertools library. " \
+                  "Like itertools and the Python3 builtins, this library uses lazy evaluation wherever possible."
     settings = "build_type", "compiler", "os", "arch"
     generators = "cmake", "cmake_find_package", "cmake_paths"
+    exports = "LICENSE.md"
 
     exports_sources = list()
     for file in os.listdir("."):
         if file.endswith(".hpp"):
             exports_sources.append(str(file))
     print("found files: " + str(exports_sources))
-
     exports_sources = tuple(exports_sources) + \
-                      ("internal/*", "CMakeLists.txt", "cmake/dummy-config.cmake.in", "LICENSE.md")
+                      ("internal/*", "CMakeLists.txt", "cmake/dummy-config.cmake.in")
     no_copy_source = True
 
     def package(self):
